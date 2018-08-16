8528d700ab5b22e9068fee64bad30e4633ddd440
David Huard <david.huard@gmail.com>
2018-08-16 14:18:53 -0400

    first version of distribution fit operator

--- dcvar/stats.py
+++ dcvar/stats.py
@@ -1,29 +1,50 @@
 # -*- coding: utf-8 -*-
 """Statistical distribution fit"""
 
-import numpy as np
 import xarray as xr
+import dask
 from scipy import stats
+
 
 def fit(arr, dist='norm'):
     """Fit an array to a distribution along the time dimension."""
+
+    # Note: stats.dist.shapes: comma separated names of shape parameters
+
     dc = getattr(stats, dist, None)
     if dc is None:
         raise ValueError("Statistical distribution {} is not in scipy.stats.".format(dist))
 
-    # Fit the parameters
-    params = np.apply_along_axis(dc.fit, arr.get_axis_num('time'), arr.data)
+    # Fit the parameters (lazy computation)
+    data = dask.array.apply_along_axis(dc.fit, arr.get_axis_num('time'), arr)
+
+    # Create a DataArray with the desired dimensions to copy them over to the parameter array.
+    mean = arr.mean(dim='time', keep_attrs=True)
+    coords = dict(mean.coords.items())
+    coords['dparams'] = ([] if dc.shapes is None else dc.shapes.split(',')) + ['loc', 'scale']
+
+    # TODO: add time and time_bnds coordinates
+    # time.attrs['climatology'] = 'climatology_bounds'
+    # coords['time'] =
+    # coords['climatology_bounds'] =
+
+    out = xr.DataArray(data=data, coords=coords, dims=(u'dparams',) + mean.dims)
+    out.attrs = arr.attrs
+    out.attrs['original_name'] = arr.standard_name
+    out.attrs['standard_name'] = '{0} distribution parameters'.format(dist)
+    out.attrs['long_name'] = '{0} distribution parameters for {1}'.format(dist, arr.standard_name)
+    out.attrs['estimator'] = 'Maximum likelihood'
+    out.attrs['cell_methods'] += ' time: fit'
+    out.attrs['units'] = ''
+    out.attrs['history'] += 'Data fitted with {0} statistical distribution using a Maximum Likelihood Estimator'
+
+    return out
+
+def test():
+    fn = '~/src/flyingpigeon/flyingpigeon/tests/testdata/cmip3/tas.sresb1.giss_model_e_r.run1.atm.da.nc'
+    D = xr.open_dataset(fn, chunks={'lat': 1}, decode_cf=True)
+    p = fit(D.tas)
+    c = stats.norm.cdf(.99, *p.values)
 
 
-#    out = xr.apply_ufunc(lambda x: np.apply_along_axis(dc.fit, -1, x,),
-#                         arr,
-#                         input_core_dims=[['time']],
-#                         output_core_dims=(('params')),
-#                         keep_attrs=True,
-#                         dask='allowed',
-#                         )
-    return xr.DataArray(params, )
 
-
-    # Create a DataArray with the parameters
-#    return params
