a9ea6698b131c37f263bbb715530483f3f518cab
Trevor James Smith <trevor_smith@live.com>
2018-09-14 12:05:22 -0400

    Added pandas to requirements and some PEP8 fixes

--- tests/test_indices.py
+++ tests/test_indices.py
@@ -17,24 +17,25 @@ the results in a reference netcdf dataset. We could then compare the hailstorm o
 of defense.
 """
 
-import pytest
-import pandas as pd
-import numpy as np
-from xclim.indices import *
-import xarray as xr
 import os
+
+import pandas as pd
+import pytest
+
+from xclim.indices import *
 
 TESTS_HOME = os.path.abspath(os.path.dirname(__file__))
 TESTS_DATA = os.path.join(TESTS_HOME, 'testdata')
 K2C = 273.15
 
+
 class Test_consecutive_frost_days():
     def time_series(self, values):
         coords = pd.date_range('7/1/2000', periods=len(values), freq=pd.DateOffset(days=1))
         return xr.DataArray(values, coords=[coords, ], dims='time',
-                         attrs={'standard_name': 'air_temperature',
-                                'cell_methods': 'time: minimum within days',
-                                'units': 'K'})
+                            attrs={'standard_name': 'air_temperature',
+                                   'cell_methods': 'time: minimum within days',
+                                   'units': 'K'})
 
     def test_one_freeze_day(self):
         a = self.time_series(np.array([3, 4, 5, -1, 3]) + K2C)
@@ -53,13 +54,14 @@ class Test_consecutive_frost_days():
         cfd = consecutive_frost_days(a)
         assert cfd == 365
 
+
 class Test_cooling_degree_days():
     def time_series(self, values):
         coords = pd.date_range('7/1/2000', periods=len(values), freq=pd.DateOffset(days=1))
         return xr.DataArray(values, coords=[coords, ], dims='time',
-                         attrs={'standard_name': 'air_temperature',
-                                'cell_methods': 'time: mean within days',
-                                'units': 'K'})
+                            attrs={'standard_name': 'air_temperature',
+                                   'cell_methods': 'time: mean within days',
+                                   'units': 'K'})
 
     def test_no_cdd(self):
         a = self.time_series(np.array([10, 15, -5, 18]) + K2C)
@@ -79,24 +81,23 @@ class Test_cooling_degree_days():
         assert cdd.units == 'K*day'
         assert len(cdd.description) > 0
 
+
 # I'd like to parametrize some of these tests so we don't have to write individual tests for each indicator.
 @pytest.mark.skip('')
 class TestTG():
-    def test_cmip3(self, cmip3_day_tas): # This fails, xarray chokes on the time dimension. Unclear why.
+    def test_cmip3(self, cmip3_day_tas):  # This fails, xarray chokes on the time dimension. Unclear why.
         rd = TG(cmip3_day_tas)
 
     def compare_against_icclim(self, cmip3_day_tas):
         pass
 
+
 @pytest.fixture(scope="session")
 def cmip3_day_tas():
-    #xr.set_options(enable_cftimeindex=False)
+    # xr.set_options(enable_cftimeindex=False)
     ds = xr.open_dataset(os.path.join(TESTS_DATA, 'cmip3', 'tas.sresb1.giss_model_e_r.run1.atm.da.nc'))
     yield ds.tas
     ds.close()
-
-
-
 
 
 @pytest.fixture
