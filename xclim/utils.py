7fd4f195b2e0e171147fa2cebe355a8cb09aea09
Sébastien Biner <sbiner@gmail.com>
2018-09-26 16:01:55 -0400

    initial version of daily resampler with test

---
+++ xclim/utils.py
@@ -0,0 +1,55 @@
+import xarray as xr
+import pandas as pd
+import cftime
+import numpy as np
+
+
+def daily_downsampler(da, freq='YS'):
+    """
+    :param da: xarray.DataArray
+
+    :param freq: string
+
+    :return: xarray.DataArray
+    """
+
+    # generate tags from da.time and freq
+    if isinstance(da.time.values[0], np.datetime64):
+        years = ['{:04n}'.format(y) for y in da.time.dt.year.values]
+        months = ['{:02n}'.format(m) for m in da.time.dt.month.values]
+    else:
+        # cannot use year, month, season attributes, not available for all calendars ...
+        years = ['{:04n}'.format(v.year) for v in da.time.values]
+        months = ['{:02n}'.format(v.month) for v in da.time.values]
+    seasons = ['DJF DJF MAM MAM MAM JJA JJA JJA SON SON SON DJF'.split()[int(m) - 1] for m in months]
+
+    n_t = da.time.size
+    if freq == 'YS':
+        # year start frequency
+        l_tags = years
+    elif freq == 'MS':
+        # month start frequency
+        l_tags = [years[i] + months[i] for i in range(n_t)]
+    elif freq == 'QS-DEC':
+        # DJF, MAM, JJA, SON seasons
+        # construct tags from list of season+year, increasing year for December
+        ys = []
+        for i in range(n_t):
+            m = months[i]
+            s = seasons[i]
+            y = years[i]
+            if m == '12':
+                y = str(int(y) + 1)
+            ys.append(y + s)
+        l_tags = ys
+    else:
+        raise RuntimeError('freqency {:s} not implemented'.format(freq))
+
+    # add tags to buffer DataArray
+    buffer = da.copy()
+    buffer.coords['tags'] = ('time', l_tags)
+
+    # return groupby according to tags
+    return buffer.groupby('tags')
+
+
