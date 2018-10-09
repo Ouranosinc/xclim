6d0758a5c4ee824ff15b2900888c52380b68173e
David Huard <david.huard@gmail.com>
2018-10-09 16:52:23 -0400

    added TxMin class

--- tests/test_temperature.py
+++ tests/test_temperature.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pytest
 import xarray as xr
 
-from xclim.temperature import TxMax
+from xclim.temperature import TxMax, TxMin
 
 
 @pytest.fixture
@@ -23,4 +23,11 @@ class Test_TxMax():
     def test_simple(self, tas_series):
         ts = tas_series(np.arange(720))
         tx_obj = TxMax()
+        tx_obj(ts, freq='Y')
+
+
+class Text_TxMin():
+    def test_simple(self, tas_series):
+        ts = tas_series(np.arange(720))
+        tx_obj = TxMin()
         tx_obj(ts, freq='Y')
--- xclim/temperature.py
+++ xclim/temperature.py
@@ -28,3 +28,13 @@ class TxMax(Indicator):
         """An aggregated value is missing if any value in the group is missing."""
         g = tas.notnull().resample(time=freq)
         return g.sum(dim='time')
+
+
+class TxMin(TxMax):
+    identifier = 'tx_min'
+    long_name = 'Minimum maximum temperature'
+    standard_name = 'tx_min'
+    description = 'Minimum daily maximum temperature over the period'
+
+    def compute(self, tasmax, freq='YS'):
+        return indices.tx_min(tasmax, freq)
