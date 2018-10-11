c623fde810a795225a2bb9c1de5c9a962d051718
sebastien biner <sbiner@gmail.com>
2018-10-11 11:59:59 -0400

    work version of growing degree days and frost days with related tests

--- tests/test_indices.py
+++ tests/test_indices.py
@@ -127,6 +127,62 @@ class Test_cooling_degree_days():
         assert cdd.description
 
 
+class Test_frost_days():
+    nc_file = 'testdata/NRCANdaily/nrcan_canada_daily_tasmin_1990.nc'
+
+    def test_3d_data_with_nans(self):
+        # test with 3d data
+        tasmin = xr.open_dataset(self.nc_file).tasmin
+        # put a nan somewhere
+        tasmin.values[180, 1, 0] = np.nan
+
+        # compute with both skipna options
+        thresh = 273.16
+        fd = xci.frost_days(tasmin, thresh=thresh, freq='YS', skipna=False)
+        fds = xci.frost_days(tasmin, thresh=thresh, freq='YS', skipna=True)
+
+        x1 = tasmin.values[:, 0, 0]
+        x2 = tasmin.values[:, 1, 0]
+
+        fd1 = (x1[x1 < thresh]).size
+        fd2 = (x2[x2 > thresh]).size
+
+        assert (np.allclose(fd1, fd.values[0, 0, 0]))
+        assert (np.allclose(fd1, fds.values[0, 0, 0]))
+        assert (np.isnan(fd.values[0, 1, 0]))
+        assert (np.allclose(fd2, fds.values[0, 1, 0]))
+        assert (np.isnan(fd.values[0, -1, -1]))
+        assert (np.isnan(fds.values[0, -1, -1]))
+
+
+class Test_growing_degree_days():
+    nc_file = 'testdata/NRCANdaily/nrcan_canada_daily_tasmax_1990.nc'
+
+    def test_3d_data_with_nans(self):
+        # test with 3d data
+        tas = xr.open_dataset(self.nc_file).tasmax
+        # put a nan somewhere
+        tas.values[180, 1, 0] = np.nan
+
+        # compute with both skipna options
+        thresh = 277.16
+        gdd = xci.growing_degree_days(tas, thresh=thresh, freq='YS', skipna=False)
+        gdds = xci.growing_degree_days(tas, thresh=thresh, freq='YS', skipna=True)
+
+        x1 = tas.values[:, 0, 0]
+        x2 = tas.values[:, 1, 0]
+
+        gdd1 = (x1[x1 > thresh] - thresh).sum()
+        gdd2 = (x2[x2 > thresh] - thresh).sum()
+
+        assert (np.allclose(gdd1, gdd.values[0, 0, 0]))
+        assert (np.allclose(gdd1, gdds.values[0, 0, 0]))
+        assert (np.isnan(gdd.values[0, 1, 0]))
+        assert (np.allclose(gdd2, gdds.values[0, 1, 0]))
+        assert (np.isnan(gdd.values[0, -1, -1]))
+        assert (np.isnan(gdds.values[0, -1, -1]))
+
+
 class Test_prcptotal():
     # build test data for different calendar
     time_std = pd.date_range('2000-01-01', '2010-12-31', freq='D')
