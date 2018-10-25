d2552bed841a8260282213ea380f3e3d4df60e99
David Huard <david.huard@gmail.com>
2018-10-25 15:03:38 -0400

    docs

--- xclim/temperature.py
+++ xclim/temperature.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Indicator and data consistency checking module
+Indicator calculation instances
+===============================
+
+While the `indices` module stores the computing functions, this module defines Indicator classes and instances that
+include a number of functionalities, such as input validation, unit conversion, output meta-data handling,
+and missing value masking.
+
+The concept followed here is to define UnivariateIndicator subclasses for each input variable, then create instances
+for each indicator.
+
 """
 
 from . import checks
