19f98943194e708c85028896abdb2ef6e379e2bc
Travis Logan <logan.travis@ouranos.ca>
2018-11-16 14:49:24 -0500

    Add indicator classes (#111)
    * Add temperature indicator classes and tests
    
    * fixed missing_any
    
    * Add temperature indicator classes and tests
    
    * Add temperature indicator classes and tests
    
    * Add temperature indicator classes and tests
    
    * code reformat..
    
    * code reformat..
    
    * flake 8
    
    lin etoo long
    
    * fixed identifier with tag
    
    * dynamic variable names

--- xclim/temperature.py
+++ xclim/temperature.py
@@ -16,6 +16,7 @@ from . import checks
 from . import indices as _ind
 from .utils import Indicator
 
+
 # TODO: Should we reference the standard vocabulary we're using ?
 # E.g. http://vocab.nerc.ac.uk/collection/P07/current/BHMHISG2/
 
@@ -62,7 +63,17 @@ heat_wave_frequency = TasminTasmax(identifier='heat_wave_frequency',
                                    standard_name='events',
                                    description="Number of spells meeting criteria for health impacting heat wave.",
                                    keywords="health,",
-                                   compute=_ind.heat_wave_frequency)
+                                   compute=_ind.heat_wave_frequency,
+                                   )
+
+heat_wave_index = Tasmax(identifier='hw_index{thresh}',
+                         units='days',
+                         description='Number of days that are part of a heatwave, '
+                                     'defined as five or more consecutive days over {thresh}℃',
+                         long_name='Number of days that are part of a heatwave',
+                         short_name='heat_wave_index',
+                         compute=_ind.heat_wave_index,
+                         )
 
 tmmean = Tas(identifier='tmmean',
              units='K',
@@ -70,8 +81,7 @@ tmmean = Tas(identifier='tmmean',
              standard_name="air_temperature",
              description="{freq} mean of daily mean temperature.",
              keywords='',
-             compute=_ind.tg_mean,)
-
+             compute=_ind.tg_mean, )
 
 tx_max = Tasmax(identifier='tx_max',
                 required_units='K',
@@ -82,12 +92,37 @@ tx_max = Tasmax(identifier='tx_max',
                 compute=_ind.tx_max,
                 )
 
+consecutive_frost_days = Tasmin(identifier='consecutive_frost_days',
+                                standard_name='spell_length_of_days_with_air_temperature_below_threshold',
+                                long_name='Maximum number of consecutive days with Tmin < 0C',
+                                units='days',
+                                cell_methods='time: min within days time: maximum over days',
+                                compute=_ind.consecutive_frost_days,
+                                )
 
 cold_spell_duration = Tasmin(identifier='cold_spell_duration',
                              standard_name='cold_spell_duration_index',
                              units='days',
                              compute=_ind.cold_spell_duration_index,
                              )
+
+cold_spell_index = Tas(identifier='cs_index{thresh}',
+                       standard_name='cold_spell_index',
+                       long_name='cold spell index',
+                       units='days',
+                       description='{freq} number of days that are part of a cold spell, defined as {window} '
+                                   'or more consecutive days with mean daily '
+                                   'temperature below  {thresh} °C',
+                       compute=_ind.cold_spell_index,
+                       )
+
+daily_freezethaw_cycles = TasminTasmax(identifier='dly_frzthw',
+                                       standard_name='daily_freezethaw_cycles',
+                                       long_name='daily freezethaw cycles',
+                                       description='Number of days with a diurnal freeze-thaw cycle '
+                                                   ': Tmax > 0℃ and Tmin < 0℃',
+                                       compute=_ind.daily_freezethaw_cycles,
+                                       )
 
 tx_min = Tasmax(identifier='tx_min',
                 long_name='Minimum maximum temperature',
@@ -97,11 +132,28 @@ tx_min = Tasmax(identifier='tx_min',
                 compute=_ind.tx_min,
                 )
 
-cooling_dd = Tas(identifier='cooling_dd',
-                 long_name='cooling degree days above {thresh}',
-                 standard_name='cooling degree days above {thresh}',
+cooling_dd = Tas(identifier='cddcold{thresh}',
+                 long_name='Cooling Degree Days (Tmean > {thresh}C)',
+                 standard_name='integral_of_air_temperature_excess_wrt_time',
                  units='K days',
+                 cell_methods='time: mean within days time: sum over days',
                  compute=_ind.cooling_degree_days,
+                 )
+
+heating_dd = Tas(identifier='hddheat{thresh}',
+                 long_name='Heating Degree Days (Tmean < {thresh}C)',
+                 standard_name='integral_of_air_temperature_deficit_wrt_time',
+                 units='K days',
+                 cell_methods='time: mean within days time: sum over days',
+                 compute=_ind.heating_degree_days,
+                 )
+
+growing_dd = Tas(identifier='gddgrow{thresh}',
+                 standard_name='integral_of_air_temperature_excess_wrt_time',
+                 long_name='growing degree days above {thresh}',
+                 units='K days',
+                 cell_methods='time: mean within days time: sum over days',
+                 compute=_ind.growing_degree_days,
                  )
 
 frost_days = Tasmin(identifier='frost_days',
@@ -110,8 +162,3 @@ frost_days = Tasmin(identifier='frost_days',
                     units='days',
                     compute=_ind.frost_days,
                     )
-
-growing_degree_days = Tas(identifier='growing_degree_days',
-                          units='K days',
-                          compute=_ind.growing_degree_days,
-                          )
