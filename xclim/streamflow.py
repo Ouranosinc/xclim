e93b48acacaeb0c720b1e5c3e462781673d85bac
David Huard <huard.david@ouranos.ca>
2019-01-23 15:57:47 -0500

    started work on streamflow indices

--- xclim/streamflow.py
+++ xclim/streamflow.py
@@ -3,19 +3,24 @@ from . import indices as _ind
 from .utils import Indicator
 
 
-class BaseFlowIndex(Indicator):
-    identifier = 'tx_max'
-    units = 'm3 s-1'
-    required_units = 'K'
-    long_name = 'streamflow'  # discharge ?
-    standard_name = 'water_volume_transport_in_river_channel'
-    description = 'Maximum daily maximum temperature over period.'
-    keywords = ''
-
-    compute = _ind.base_flow_index
+class Streamflow(Indicator):
+    required_units = 'm3 s-1'
+    context = 'hydro'
 
     def cfprobe(self, q):
         checks.check_valid(q, 'standard_name', 'water_volume_transport_in_river_channel')
 
-    def validate(self, q):
-        checks.assert_daily(q)
+
+base_flow_index = Streamflow(identifier='base_flow_index',
+                             units='',
+                             standard_name='',
+                             compute=ind.base_flow_index)
+
+q_max = Streamflow(identifier='q_max',
+                   units='m3 s-1',
+                   standard_name='water_volume_transport_in_river_channel',
+                   compute=ind.
+                   )
+
+
+
