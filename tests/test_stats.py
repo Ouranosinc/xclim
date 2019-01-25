109e49820bf5941b48fa6e76526dc39302b34b17
David Huard <huard.david@ouranos.ca>
2019-01-25 17:02:25 -0500

    added frequency analysis function and a simple test

---
+++ tests/test_stats.py
@@ -0,0 +1,44 @@
+import pytest
+from xclim import stats
+import numpy as np
+import xarray as xr
+from scipy.stats import lognorm
+
+
+class TestStats(object):
+
+    def setup(self):
+        self.nx, self.ny = 2, 3
+        x = np.arange(0, self.nx)
+        y = np.arange(0, self.ny)
+
+        cx = xr.IndexVariable('x', x)
+        cy = xr.IndexVariable('y', y)
+        time = xr.IndexVariable('time', np.arange(50))
+
+        self.da = xr.DataArray(np.random.lognormal(10, 1, (len(time), self.nx, self.ny)),
+                               dims=('time', 'x', 'y'),
+                               coords={'time': time, 'x': cx, 'y': cy}
+                               )
+
+    def test_fit(self):
+        p = stats.fit(self.da, 'lognorm')
+
+        assert p.dims[0] == 'dparams'
+        assert p.get_axis_num('dparams') == 0
+        p0 = lognorm.fit(self.da.values[:, 0, 0])
+        np.testing.assert_array_equal(p[:, 0, 0], p0)
+
+
+        # Check that we can reuse the parameters with scipy distributions
+        cdf = lognorm.cdf(.99, *p.values)
+        assert cdf.shape == (self.nx, self.ny)
+
+
+    def test_fa(self):
+        T = 10
+        q = stats.fa(self.da, T, 'lognorm')
+
+        p0 = lognorm.fit(self.da.values[:, 0, 0])
+        q0 = lognorm.ppf(1-1./T, *p0)
+        np.testing.assert_array_equal(q[0, 0], q0)
--- xclim/stats.py
+++ xclim/stats.py
@@ -2,31 +2,43 @@
 """
 Statistical distribution fit module
 """
+# Note: stats.dist.shapes: comma separated names of shape parameters
+# The other parameters, common to all distribution, are loc and scale.
 
 import dask
 import xarray as xr
-from scipy import stats
+
+
+def get_dist(dist):
+    """Return a distribution object from scipy.stats.
+    """
+    from scipy import stats
+
+    dc = getattr(stats, dist, None)
+    if dc is None:
+        e = "Statistical distribution `{}` is not in scipy.stats.".format(dist)
+        raise ValueError(e)
+    return dc
 
 
 def fit(arr, dist='norm'):
-    """Fit an array to a distribution along the time dimension.
+    """Fit an array to a univariate distribution along the time dimension.
 
     Parameters
     ----------
     arr : xarray.DataArray
       Time series to be fitted along the time dimension.
     dist : str
-      Name of the distribution (see scipy.stats)
+      Name of the univariate distribution, such as beta, expon, genextreme, gamma, gumbel_r, lognorm, norm
+      (see scipy.stats).
+
+    Returns
+    -------
+    xarray.DataArray
+      An array of distribution parameters fitted using the method of Maximum Likelihood.
     """
-
-    # Note: stats.dist.shapes: comma separated names of shape parameters
-    # The other parameters, common to all distribution, are loc and scale.
-
-    # Get the distribution object
-    dc = getattr(stats, dist, None)
-    if dc is None:
-        e = "Statistical distribution {} is not in scipy.stats.".format(dist)
-        raise ValueError(e)
+    # Get the distribution
+    dc = get_dist(dist)
 
     # Fit the parameters (lazy computation)
     data = dask.array.apply_along_axis(dc.fit, arr.get_axis_num('time'), arr)
@@ -38,27 +50,60 @@ def fit(arr, dist='norm'):
     coords = dict(mean.coords.items())
     coords['dparams'] = ([] if dc.shapes is None else dc.shapes.split(',')) + ['loc', 'scale']
 
-    # TODO: add time and time_bnds coordinates
+    # TODO: add time and time_bnds coordinates (Low will work on this)
     # time.attrs['climatology'] = 'climatology_bounds'
     # coords['time'] =
     # coords['climatology_bounds'] =
 
     out = xr.DataArray(data=data, coords=coords, dims=(u'dparams',) + mean.dims)
     out.attrs = arr.attrs
-    out.attrs['original_name'] = arr.standard_name
+    out.attrs['original_name'] = getattr(arr, 'standard_name', '')
     out.attrs['standard_name'] = '{0} distribution parameters'.format(dist)
-    out.attrs['long_name'] = '{0} distribution parameters for {1}'.format(dist, arr.standard_name)
+    out.attrs['long_name'] = '{0} distribution parameters for {1}'.format(dist, getattr(arr, 'standard_name', ''))
     out.attrs['estimator'] = 'Maximum likelihood'
-    out.attrs['cell_methods'] += ' time: fit'
+    out.attrs['cell_methods'] = (out.attrs.get('cell_methods', '') + ' time: fit').strip()
     out.attrs['units'] = ''
-    out.attrs['history'] += 'Data fitted with {0} statistical distribution using a Maximum Likelihood Estimator'
+    out.attrs['history'] = out.attrs.get('history', '') + \
+                           'Data fitted with {0} statistical distribution using a Maximum Likelihood ' \
+                           'Estimator'.format(dist)
 
     return out
 
 
-# FIXME: Write a name for this test
-def test():
-    fn = '~/src/flyingpigeon/flyingpigeon/tests/testdata/cmip3/tas.sresb1.giss_model_e_r.run1.atm.da.nc'
-    D = xr.open_dataset(fn, chunks={'lat': 1}, decode_cf=True)
-    p = fit(D.tas)
-    stats.norm.cdf(.99, *p.values)
+def fa(arr, T, dist='norm', mode='high'):
+    """Return the value corresponding to the given return period.
+
+    Parameters
+    ----------
+    arr : xarray.DataArray
+      Input data with a `time` dimension.
+    T : int or sequence
+      Return period. The period depends on the resolution of the input data. If the input array's resolution is
+      yearly, then the return period is in years.
+    dist : str
+      Name of the univariate distribution, such as beta, expon, genextreme, gamma, gumbel_r, lognorm, norm
+      (see scipy.stats).
+    mode : {'low', 'high'}
+      Whether we are looking for a probability of exceedance (high) or a probability of non-exceedance (low).
+
+    Returns
+    -------
+    xarray.DataArray
+      An array of values with a 1/T probability of exceedance (if mode=='high').
+    """
+
+    # Get the distribution
+    dc = get_dist(dist)
+
+    p = fit(arr, dist)
+
+    if mode == 'high':
+        func = lambda x: dc.isf(1./T, *x)
+    elif mode == 'low':
+        func = lambda x: dc.ppf(1./T, *x)
+    else:
+        raise ValueError("mode `{}` should be either 'high' or 'low'".format(mode))
+
+    data = dask.array.apply_along_axis(func, p.get_axis_num('dparams'), p)
+    # TODO: Update attributes
+    return data
