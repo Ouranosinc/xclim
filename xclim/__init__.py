8b525e14642ad9d64615517d31bb71b0f96d3972
Zeitsperre <trevor_smith@live.com>
2019-02-06 14:51:33 -0500

    Ready to tag beta

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -9,7 +9,7 @@ from . import indices
 
 __author__ = """Travis Logan"""
 __email__ = 'logan.travis@ouranos.ca'
-__version__ = '0.8-beta'
+__version__ = '0.7-beta'
 
 
 def build_module(name, objs, doc='', source=None, mode='ignore'):
