ed608d542b6e2eb93d010d1bd17e6977dc61a17b
Travis Logan <logan.travis@ouranos.ca>
2019-02-18 15:39:36 -0500

    Subset utilities (#162)
    * subset bbox and gridpoints funcs
    
    * Subset tests added
    
    * test dataset subsetting and docs strings and examples
    
    * calculate grid point distance using pyproj geodeisc.  Add start/end year inputs individually
    
    * added pyproj dependency
    
    * docstring update - start / end year

--- xclim/utils.py
+++ xclim/utils.py
@@ -13,6 +13,7 @@ from inspect2 import signature, _empty
 import abc
 from collections import defaultdict
 import datetime as dt
+from pyproj import Geod
 
 from boltons.funcutils import wraps
 
@@ -122,6 +123,162 @@ def percentile_doy(arr, window=5, per=.1):
         p = adjust_doy_calendar(p.loc[p.dayofyear < 366], arr)
 
     return p
+
+
+def subset_bbox(da, lon_bnds='', lat_bnds='', start_yr='', end_yr=''):
+    """Subset a datarray or dataset spatially (and temporally) using a lat lon bounding box and years selection
+
+        Return a subsetted data array for grid points falling within a spatial bounding box
+        defined by longitude and latitudinal bounds and for years falling within provided year bounds
+
+        Parameters
+        ----------
+        arr : xarray.DataArray or xarray.Dataset
+          Input data.
+        lon_bnds (optional) : list of floats
+          List of maximum and minimum longitudinal bounds. Defaults to all longitudes in original data-array
+        lat_bnds (optional) :  list of floats
+          List maximum and minimum latitudinal bounds.  Defaults to all latitudes in original data-array
+        year_bnds (optional) :  list of floats or float
+          List of maximum and minimum year bounds. If single float returns single year only.
+          Defaults to all years in original data-array
+
+
+        Returns
+        -------
+        xarray.DataArray or xarray.DataSet
+          subsetted data array or dataset
+
+        Examples
+        --------
+
+        >>> from xclim import utils
+        >>> ds = xr.open_dataset('pr.day.nc')
+        Subset lat lon and years
+        >>> prSub = utils.subset_bbox(ds.pr, lon_bnds=[-75,-70],lat_bnds=[40,45],start_yr=1990,end_yr=1999)
+        Subset data array lat, lon and single year
+        >>> prSub = utils.subset_bbox(ds.pr, lon_bnds=[-75,-70],lat_bnds=[40,45],start_yr=1990,end_yr=1990)
+        Subset dataarray single year keep entire lon, lat grid
+        >>> prSub = utils.subset_bbox(ds.pr,start_yr=1990,end_yr=1990) # one year only entire grid
+        Subset mutliple variables in a single dataset
+        >>> ds = xr.open_mfdataset(['pr.day.nc','tas.day.nc'])
+        >>> dsSub = utils.subset_bbox(ds,lon_bnds=[-75,-70],lat_bnds=[40,45],start_yr=1990,end_yr=1999)
+        """
+
+    if lon_bnds:
+
+        lon_bnds = np.asarray(lon_bnds)
+
+        # adjust for files with all postive longitudes if necessary
+        if np.all(da.lon > 0) and np.any(lon_bnds < 0):
+            lon_bnds[lon_bnds < 0] += 360
+
+        lon_cond = (da.lon >= lon_bnds.min()) & (da.lon <= lon_bnds.max())
+    else:
+        lon_cond = (da.lon >= da.lon.min()) & (da.lon <= da.lon.max())
+
+    if lat_bnds:
+        lat_bnds = np.asarray(lat_bnds)
+        lat_cond = (da.lat >= lat_bnds.min()) & (da.lat <= lat_bnds.max())
+    else:
+        lat_cond = (da.lat >= da.lat.min()) & (da.lat <= da.lat.max())
+
+    if start_yr or end_yr:
+        if not start_yr:
+            start_yr = da.time.dt.year.min()
+        if not end_yr:
+            end_yr = da.time.dt.year.max()
+
+        year_bnds = np.asarray([start_yr, end_yr])
+        if len(year_bnds) == 1:
+            time_cond = da.time.dt.year == year_bnds
+        else:
+            time_cond = (da.time.dt.year >= year_bnds.min()) & (da.time.dt.year <= year_bnds.max())
+    else:
+        time_cond = (da.time.dt.year >= da.time.dt.year.min()) & (da.time.dt.year <= da.time.dt.year.max())
+
+    return da.where(lon_cond & lat_cond & time_cond, drop=True)
+
+
+def subset_gridpoint(da, lon, lat, start_yr='', end_yr=''):
+    """Extract a nearest gridpoint from datarray based on lat lon coordinate.
+    Time series can optionally be subsetted by year(s)
+
+            Return a subsetted data array (or dataset) for the grid point falling nearest the input
+            longitude and latitudecoordinates. Optionally subset the data array for years falling
+            within provided year bounds
+
+            Parameters
+            ----------
+            da : xarray.DataArray or xarray.DataSet
+              Input data.
+            lon : float
+              longitude coordinate.
+            lat:  float
+              latitude coordinate.
+            year_bnds (optional) :  list of floats or float
+              List of maximum and minimum year bounds. If single float returns one year only.
+              Defaults to all years in original data-array
+
+
+            Returns
+            -------
+            xarray.DataArray or xarray.DataSet
+              subsetted data array or dataset
+
+        Examples
+        --------
+
+        >>> from xclim import utils
+        >>> ds = xr.open_dataset('pr.day.nc')
+        Subset lat lon point and multiple years
+        >>> prSub = utils.subset_gridpoint(ds.pr, lon=-75,lat=45,start_yr=1990,end_yr=1999)
+        Subset lat, lon point and single year
+        >>> prSub = utils.subset_gridpoint(ds.pr, lon=-75,lat=45,start_yr=1990,end_yr=1990)
+         Subset mutliple variables in a single dataset
+        >>> ds = xr.open_mfdataset(['pr.day.nc','tas.day.nc'])
+        >>> dsSub = utils.subset_gridpoint(ds, lon=-75,lat=45,start_yr=1990,end_yr=1999)
+            """
+
+    g = Geod(ellps='WGS84')  # WGS84 ellipsoid - decent globaly
+    # adjust for files with all postive longitudes if necessary
+    if np.all(da.lon > 0) and lon < 0:
+        lon += 360
+
+    if len(da.lon.shape) == 1 & len(da.lat.shape) == 1:
+        # create a 2d grid of lon, lat values
+        lon1, lat1 = np.meshgrid(np.asarray(da.lon.values), np.asarray(da.lat.values))
+
+    else:
+        lon1 = da.lon.values
+        lat1 = da.lat.values
+    shp_orig = lon1.shape
+    lon1 = np.reshape(lon1, (lon1.size))
+    lat1 = np.reshape(lat1, (lat1.size))
+    # calculate geodesic distance between grid points and point of interest
+    az12, az21, dist = g.inv(lon1, lat1, np.broadcast_to(lon, lon1.shape), np.broadcast_to(lat, lat1.shape))
+    dist = dist.reshape(shp_orig)
+
+    iy, ix = np.unravel_index(np.argmin(dist, axis=None), dist.shape)
+    xydims = [x for x in da.dims if 'time' not in x]
+    args = {}
+    args[xydims[0]] = iy
+    args[xydims[1]] = ix
+    out = da.isel(**args)
+    if start_yr or end_yr:
+        if not start_yr:
+            start_yr = da.time.dt.year.min()
+        if not end_yr:
+            end_yr = da.time.dt.year.max()
+        year_bnds = np.asarray([start_yr, end_yr])
+
+        if len(year_bnds) == 1:
+            time_cond = da.time.dt.year == year_bnds
+        else:
+            time_cond = (da.time.dt.year >= year_bnds.min()) & (da.time.dt.year <= year_bnds.max())
+        out = out.where(time_cond, drop=True)
+
+    return out
 
 
 # TODO: I'd like this function to use calendar instead of target (ie target calendar.)
@@ -506,7 +663,7 @@ def parse_doc(doc):
             out['title'], out['abstract'] = content
 
     for i in range(0, len(sections), 2):
-        header, content = sections[i:i+2]
+        header, content = sections[i:i + 2]
 
         if header in ['Notes', 'References']:
             out[header.lower()] = content.replace('\n    ', '\n')
