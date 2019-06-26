e9282b7f938a61cbdd5534f5136a0829923722bf
Zeitsperre <trevor_smith@live.com>
2019-06-26 10:53:41 -0400

    Setting HOME in docs

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup_requirements = ["pytest-runner"]
 
 test_requirements = ["pytest", "tox"]
 
-docs_requirements = ["sphinx", "guzzle-sphinx-theme", "nbsphinx", "pypandoc"]
+docs_requirements = ["sphinx", "guzzle-sphinx-theme", "nbsphinx", "pandoc"]
 
 KEYWORDS = "xclim climate climatology netcdf gridded analysis"
 
