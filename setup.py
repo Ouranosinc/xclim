361d4219a521de149344bd7920d99a2151fbbe4e
Zeitsperre <trevor_smith@live.com>
2019-08-01 16:08:23 -0400

    OS independent package setup notifier

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
