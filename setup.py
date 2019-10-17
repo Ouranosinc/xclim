51773e4678fb5e64eda93ab9849ed58b365524e9
Travis Logan <logan.travis@ouranos.ca>
2019-10-17 08:33:43 -0400

    Fix 263new (#280)
    * return fig_data dictionary for separate plotting; Add plot_rsqprofile(fig_data);
    
    * include sklearn
    
    * modify create_ensemble();  docstrings
    
    * pin xarray to 0.13.0 temporarily
    
    * update hist
    
    * Added type hinting and docstring changes

--- setup.py
+++ setup.py
@@ -27,10 +27,11 @@ requirements = [
     "netCDF4>=1.4",
     "dask[complete]",
     "bottleneck>=1.2.1",
-    "xarray>=0.12.2",
+    "xarray==0.13.0",
     "pyproj>=1.9.5.1",
     "pint>=0.8",
     "boltons>=19.0",
+    "scikit-learn>=0.21.3",
 ]
 
 setup_requirements = ["pytest-runner"]
