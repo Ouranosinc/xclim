e6aa7b448470241a58dde065141176ced273f7bc
Zeitsperre <trevor_smith@live.com>
2019-11-18 14:34:09 -0500

    bump version to 0.11.5-beta

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -9,7 +9,7 @@ from xclim import indices
 
 __author__ = """Travis Logan"""
 __email__ = "logan.travis@ouranos.ca"
-__version__ = "0.11.4"
+__version__ = "0.11.5-beta"
 
 
 def build_module(name, objs, doc="", source=None, mode="ignore"):
