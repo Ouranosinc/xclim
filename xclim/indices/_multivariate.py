3d090c71f126b5ded0a1c72d509c6b693d281074
Zeitsperre <trevor_smith@live.com>
2019-11-18 16:25:55 -0500

    Fix black

--- xclim/indices/_multivariate.py
+++ xclim/indices/_multivariate.py
@@ -1192,7 +1192,7 @@ def winter_rain_ratio(
     pr: xr.DataArray = None,
     prsn: xr.DataArray = None,
     tas: xr.DataArray = None,
-    freq: Optional[str] = None,
+    freq: Optional[str] = None
 ) -> xr.DataArray:
     """Ratio of rainfall to total precipitation during winter
 
