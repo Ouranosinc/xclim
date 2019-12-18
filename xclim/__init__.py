3ab670693b7fae2887900cb76956dfbfdc3b9dea
Trevor James Smith <Zeistperre@github.com>
2019-12-18 10:24:46 -0500

    Bumped version to 0.12.3-beta

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -9,7 +9,7 @@ from xclim import indices
 
 __author__ = """Travis Logan"""
 __email__ = "logan.travis@ouranos.ca"
-__version__ = "0.12.2"
+__version__ = "0.12.3-beta"
 
 
 def build_module(name, objs, doc="", source=None, mode="ignore"):
