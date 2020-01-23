fee5976eaaa860e96d04280cfd91e366f2f6fe50
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2020-01-23 13:04:31 -0500

    Remove FWI from data.py

--- tests/data.py
+++ tests/data.py
@@ -1,16 +1,6 @@
-from xclim.testing import TestDataSet
 import os
 from pathlib import Path
 
-TD = Path(os.path.dirname(__file__)) / 'testdata'
+from xclim.testing import TestDataSet
 
-
-FWI = TestDataSet('gfwed', TD / 'FWI')
-FWI.add('init', 'https://portal.nccs.nasa.gov/datashare/'
-                'GlobalFWI/v2.0/fwiCalcs.MERRA2/Default/MERRA2/1985/FWI.MERRA2.Daily.Default.19850730.nc'),
-FWI.add('pr', 'https://portal.nccs.nasa.gov/datashare/'
-              'GlobalFWI/v2.0/wxInput/MERRA2/1985/Prec.MERRA2.Daily.Default.19850731.nc'),
-FWI.add('wx', 'https://portal.nccs.nasa.gov/datashare/'
-              'GlobalFWI/v2.0/wxInput/MERRA2/1985/Wx.MERRA2.Daily.Default.19850731.nc'),
-FWI.add('out', 'https://portal.nccs.nasa.gov/datashare/'
-               'GlobalFWI/v2.0/fwiCalcs.MERRA2/Default/MERRA2/1985/FWI.MERRA2.Daily.Default.19850731.nc')
+TD = Path(os.path.dirname(__file__)) / "testdata"
