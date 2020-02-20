37d3df13c28f839e8bcbf8c6ce384f6b4bb9b996
Zeitsperre <trevor_smith@live.com>
2020-02-20 15:02:53 -0500

    bumped version to 0.14.0-beta

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -12,7 +12,7 @@ from xclim import seaIce
 
 __author__ = """Travis Logan"""
 __email__ = "logan.travis@ouranos.ca"
-__version__ = "0.13.4-beta"
+__version__ = "0.14.0-beta"
 
 
 def build_module(name, objs, doc="", source=None, mode="ignore"):
