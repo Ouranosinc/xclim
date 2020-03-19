66105969c0264d62ef94b34e9890d5f2266ee1dd
David Huard <huard.david@ouranos.ca>
2020-03-19 09:18:10 -0400

    added test for jitter function

--- tests/downscaling/test_utils.py
+++ tests/downscaling/test_utils.py
@@ -1,1 +1,14 @@
-# TODO
+import numpy as np
+import xarray as xr
+
+from xclim.downscaling import utils as u
+
+
+def test_jitter_under_thresh():
+    da = xr.DataArray([0.5, 2.1, np.nan])
+    out = u.jitter_under_thresh(da, 1)
+
+    assert da[0] != out[0]
+    assert da[0] < 1
+    assert da[0] > 0
+    np.testing.assert_allclose(da[1:], out[1:])
