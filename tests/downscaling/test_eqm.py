40f3bbbbc67899d1c7743c2236f24a4453471817
David Huard <huard.david@ouranos.ca>
2020-03-19 09:09:56 -0400

    basic test of eqm pass

--- tests/downscaling/test_eqm.py
+++ tests/downscaling/test_eqm.py
@@ -6,15 +6,17 @@ from xclim.downscaling import eqm
 # TODO: Just a smoke test, check that it actually does the right thing
 class TestEQM:
     def test_mon(self, mon_tas, tas_series, mon_triangular):
-        r1 = np.random.rand(10000)
-        x = tas_series(r1)
-        r2 = np.random.rand(10000)
-        y = mon_tas(r2)
+        r = 1 + np.random.rand(10000)
+        x = tas_series(r)
+        noise = np.random.rand(10000) * 1e-6
+        y = mon_tas(r + noise)
 
         # Test train
         d = eqm.train(x, y, 5, "time.month")
-        # np.testing.assert_array_almost_equal(d, mon_triangular)
+        md = d.mean(dim="x")
+        np.testing.assert_array_almost_equal(md, mon_triangular, 1)
+        # TODO: Test individual quantiles
 
         # Test predict
-        eqm.predict(x, d)
-        # np.testing.assert_array_almost_equal(p, y)
+        p = eqm.predict(x, d)
+        np.testing.assert_array_almost_equal(p, y, 3)
