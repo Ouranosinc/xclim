805d4b33990c0a102072abd61235a949455dd6e1
Trevor James Smith <trevor_smith@live.com>
2020-04-08 15:13:43 -0400

    Add tests for vectorize comparison

--- tests/test_subset.py
+++ tests/test_subset.py
@@ -603,7 +603,8 @@ class TestSubsetShape:
                     sub[vari].sel(lon=lon1, lat=lat1), ds[vari].sel(lon=lon1, lat=lat1)
                 )
 
-    def test_wraps(self, tmp_netcdf_filename):
+    @pytest.mark.parametrize("vectorize", [True, False])
+    def test_wraps(self, tmp_netcdf_filename, vectorize):
         ds = xr.open_dataset(self.nc_file)
 
         # Polygon crosses meridian, a warning should be raised
@@ -620,7 +621,7 @@ class TestSubsetShape:
         self.compare_vals(ds, sub, "tas")
 
         poly = gpd.read_file(self.meridian_multi_geojson)
-        subtas = subset.subset_shape(ds.tas, poly)
+        subtas = subset.subset_shape(ds.tas, poly, vectorize=vectorize)
         np.testing.assert_array_almost_equal(
             float(np.mean(subtas.isel(time=0))), 281.091553
         )
@@ -633,11 +634,12 @@ class TestSubsetShape:
         with xr.open_dataset(filename_or_obj=tmp_netcdf_filename) as f:
             assert {"tas", "crs"}.issubset(set(f.data_vars))
 
-    def test_no_wraps(self, tmp_netcdf_filename):
+    @pytest.mark.parametrize("vectorize", [True, False])
+    def test_no_wraps(self, tmp_netcdf_filename, vectorize):
         ds = xr.open_dataset(self.nc_file)
 
         with pytest.warns(None) as record:
-            sub = subset.subset_shape(ds, self.poslons_geojson)
+            sub = subset.subset_shape(ds, self.poslons_geojson, vectorize=vectorize)
 
         self.compare_vals(ds, sub, "tas")
 
