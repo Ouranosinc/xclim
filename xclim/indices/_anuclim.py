0f91362d5fab12fa5549d41c9f2145f66492e625
Phobos <pascal.bourgault@gmail.com>
2020-05-06 17:34:52 -0400

    Add tg wet/dry and prcp warm/cold - no tests

--- xclim/indices/_anuclim.py
+++ xclim/indices/_anuclim.py
@@ -1,3 +1,4 @@
+import numpy as np
 import xarray
 
 import xclim.indices as xci
@@ -18,6 +19,7 @@ __all__ = [
     "temperature_seasonality",
     "precip_seasonality",
     "tg_mean_warmcold_quarter",
+    "tg_mean_wetdry_quarter",
     "prcptot_wetdry_quarter",
 ]
 
@@ -205,6 +207,81 @@ def tg_mean_warmcold_quarter(
         return out
 
 
+@declare_units("[temperature]", tas="[temperature]", pr="[precipitation]")
+def tg_mean_wetdry_quarter(
+    tas: xarray.DataArray,
+    pr: xarray.DataArray,
+    op: str = None,
+    input_freq: str = None,
+    freq="YS",
+):
+    r""" ANUCLIM Mean temperature of wettest/dryest quarter
+    The wettest (or dryest) quarter of the year is determined, and the mean
+    temperature of this period is calculated.  If the input data frequency is "daily" or "weekly" quarters
+    are defined as 13 week periods, otherwise are 3 months.
+
+    Parameters
+    ----------
+    tas : xarray.DataArray
+      Mean temperature [℃] or [K] at daily, weekly, or monthly frequency
+
+    pr : xarray.DataArray
+      Total precipitation rate at daily, weekly, or monthly frequency
+
+    op : str
+        Operation to perform :  'wettest' calculate for the wettest quarter ; 'dryest' calculate for the dryest quarter
+
+    input_freq : str
+        input data time frequency - One of 'daily', 'weekly' or 'monthly'
+
+    Returns
+    -------
+    xarray.DataArray
+       mean temperature values of the wettest/dryest quarter of each year.
+
+    Notes
+    -----
+    According to the ANUCLIM user-guide https://fennerschool.anu.edu.au/files/anuclim61.pdf (ch. 6), input
+    values should be at a weekly (or monthly) frequency.  However, the xclim.indices implementation here will calculate
+    the result with input data of any frequency.
+
+    """
+    # determine input data frequency
+    if input_freq == "monthly":
+        wind = 3
+    elif input_freq == "weekly":
+        wind = 13
+    elif input_freq == "daily":
+        tas = xci.tg_mean(tas, freq="7D")
+        pr = xci.precip_accumulation(pr, freq="7D")
+        wind = 13
+    else:
+        raise NotImplementedError(
+            f'Unknown input time frequency "{input_freq}" : input_freq parameter must be '
+            f'one of "daily", "weekly" or "monthly"'
+        )
+
+    with xarray.set_options(keep_attrs=True):
+        tas_qrt = tas.rolling(time=wind, center=False).mean()
+        pr_qrt = pr.rolling(time=wind, center=False).sum()
+
+        if op == "wettest":
+            np_op = "max"
+        elif op == "dryest":
+            np_op = "min"
+        else:
+            raise NotImplementedError(
+                f'Unknown operation "{op}" ; op parameter but be one of "wettest" or "dryest"'
+            )
+        out = (
+            xarray.Dataset(data_vars={"tas": tas_qrt, "pr": pr_qrt})
+            .resample(time=freq)
+            .map(_get_from_other_extreme, args=("tas", "pr", np_op))
+        )
+        out.attrs = tas.attrs
+        return out
+
+
 @declare_units("mm", pr="[precipitation]")
 def prcptot_wetdry_quarter(
     pr: xarray.DataArray, op: str = None, input_freq: str = None
@@ -279,9 +356,104 @@ def prcptot_wetdry_quarter(
         return out
 
 
+@declare_units("mm", pr="[precipitation]", tas="[temperature]")
+def prcptot_warmcold_quarter(
+    pr: xarray.DataArray,
+    tas: xarray.DataArray,
+    op: str = None,
+    input_freq: str = None,
+    freq="YS",
+):
+    r""" ANUCLIM Total precipitation of warmest/coldest quarter
+    The warmest (or coldest) quarter of the year is determined, and the total
+    precipitation of this period is calculated.  If the input data frequency is "daily" or "weekly" quarters
+    are defined as 13 week periods, otherwise are 3 months.
+
+    Parameters
+    ----------
+    pr : xarray.DataArray
+      Total precipitation rate at daily, weekly, or monthly frequency
+
+    tas : xarray.DataArray
+      Mean temperature [℃] or [K] at daily, weekly, or monthly frequency
+
+    op : str
+        Operation to perform: 'warmest' calculate for the warmest quarter ; 'coldest' calculate for the coldest quarter
+
+    input_freq : str
+        input data time frequency - One of 'daily', 'weekly' or 'monthly'
+
+    Returns
+    -------
+    xarray.DataArray
+       Total precipitation values of the warmest/coldest quarter of each year.
+
+    Notes
+    -----
+    According to the ANUCLIM user-guide https://fennerschool.anu.edu.au/files/anuclim61.pdf (ch. 6), input
+    values should be at a weekly (or monthly) frequency.  However, the xclim.indices implementation here will calculate
+    the result with input data of any frequency.
+
+    """
+    # determine input data frequency
+    if input_freq == "monthly":
+        wind = 3
+    elif input_freq == "weekly":
+        wind = 13
+    elif input_freq == "daily":
+        tas = xci.tg_mean(tas, freq="7D")
+        pr = xci.precip_accumulation(pr, freq="7D")
+        wind = 13
+    else:
+        raise NotImplementedError(
+            f'Unknown input time frequency "{input_freq}" : input_freq parameter must be '
+            f'one of "daily", "weekly" or "monthly"'
+        )
+
+    with xarray.set_options(keep_attrs=True):
+        tas_qrt = tas.rolling(time=wind, center=False).mean()
+        pr_qrt = pr.rolling(time=wind, center=False).sum()
+
+        if op == "warmest":
+            np_op = "max"
+        elif op == "coldest":
+            np_op = "min"
+        else:
+            raise NotImplementedError(
+                f'Unknown operation "{op}" ; op parameter but be one of "warmest" or "coldest"'
+            )
+        out = (
+            xarray.Dataset(data_vars={"tas": tas_qrt, "pr": pr_qrt})
+            .resample(time=freq)
+            .map(_get_from_other_extreme, args=("pr", "tas", np_op))
+        )
+        out.attrs = pr.attrs
+        out.attrs["units"] = "mm"
+        return out
+
+
 def _anuclim_coeff_var(arr: xarray.DataArray):
     r""" calculate the annual coefficient of variation for anuclim indices"""
     cv = arr.resample(time="YS").std(dim="time") / arr.resample(time="YS").mean(
         dim="time"
     )
     return cv
+
+
+def _get_from_other_extreme(ds, var, crit, op, dim="time"):
+    if op == "max":
+        op = np.argmax
+    elif op == "min":
+        op = np.argmin
+
+    def func(var, crit):
+        return var[op(crit)]
+
+    return xarray.apply_ufunc(
+        func,
+        ds[var],
+        ds[crit],
+        input_core_dims=[[dim], [dim]],
+        vectorize=True,
+        dask="parallelized",
+    )
