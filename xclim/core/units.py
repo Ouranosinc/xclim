17e96bfa965f86170944eb3e701d3b482d715248
David Huard <huard.david@ouranos.ca>
2020-05-13 19:41:35 -0400

    feat: support units of 1

--- xclim/core/units.py
+++ xclim/core/units.py
@@ -143,6 +143,9 @@ def units2pint(value: Union[xr.DataArray, str]) -> pint.unit.UnitDefinition:
         raise NotImplementedError(f"Value of type `{type(value)}` not supported.")
 
     unit = unit.replace("%", "pct")
+    if unit == "1":
+        unit = ""
+
     try:  # Pint compatible
         return units.parse_expression(unit).units
     except (
