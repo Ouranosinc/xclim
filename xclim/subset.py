8b1802d7cdd8a8fe71289c038d4b951bb34f8b48
Trevor James Smith <trevor_smith@live.com>
2020-08-18 16:49:50 -0400

    Specify deprecation version for subset module

--- xclim/subset.py
+++ xclim/subset.py
@@ -8,7 +8,7 @@ try:
     __all__ = [x for x in __all__]
 
     warnings.warn(
-        f"{__name__} is deprecated in xclim v0.19.x. "
+        f"{__name__} is deprecated in xclim v0.19.1-beta. "
         f"Please take note that xclim presently exposes the 'clisops' library subsetting API "
         f"via `from clisops.core import subset`. This functionality may eventually change.",
         DeprecationWarning,
@@ -17,7 +17,7 @@ try:
 
 except ImportError as e:
     raise ImportError(
-        f"{__name__} is deprecated in xclim v0.19.x. "
+        f"{__name__} is deprecated in xclim v0.19.1-beta. "
         f"Subset functions are now dependent on the `clisops` library. This library can be installed via "
         f'`pip install xclim["gis"]`, `pip install clisops` or `conda install clisops`.'
     ) from e
