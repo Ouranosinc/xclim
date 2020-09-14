c4f2e26ea668ca01c12b72834a5f5ca5c81fd491
Phobos <pascal.bourgault@gmail.com>
2020-09-14 14:43:59 -0400

    Add glob in value formatting - Add french nouns

--- xclim/core/formatting.py
+++ xclim/core/formatting.py
@@ -7,6 +7,7 @@ Formatting utilities for indicators
 import datetime as dt
 import re
 import string
+from glob.fnmatch import fnmatch
 from typing import Mapping, Optional, Sequence, Union
 
 import xarray as xr
@@ -54,24 +55,32 @@ class AttrFormatter(string.Formatter):
         >>> fmt.format("Le chien est {adj1:m}, l'oie est {adj2:f}", adj1='nice', adj2='evil')
         "Le chien est beau, l'oie est méchante"
         """
-        if value in self.mapping and not format_spec:
-            return self.mapping[value][0]
+        baseval = self._match_value(value)
+        if baseval is not None and not format_spec:
+            return self.mapping[baseval][0]
 
         if format_spec in self.modifiers:
-            if value in self.mapping:
-                return self.mapping[value][self.modifiers.index(format_spec)]
+            if baseval is not None:
+                return self.mapping[baseval][self.modifiers.index(format_spec)]
             raise ValueError(
                 f"No known mapping for string '{value}' with modifier '{format_spec}'"
             )
         return super().format_field(value, format_spec)
+
+    def _match_value(self, value):
+        for mapval in self.mapping.values():
+            if fnmatch(value, mapval):
+                return mapval
+        return None
 
 
 # Tag mappings between keyword arguments and long-form text.
 default_formatter = AttrFormatter(
     {
         "YS": ["annual", "years"],
+        "AS-*": ["annual", "years"],
         "MS": ["monthly", "months"],
-        "QS-DEC": ["seasonal", "seasons"],
+        "QS-*": ["seasonal", "seasons"],
         "DJF": ["winter"],
         "MAM": ["spring"],
         "JJA": ["summer"],
