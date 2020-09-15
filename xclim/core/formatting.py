7605ec709f4102e75cb2cde6bd294d399e20cf44
Phobos <pascal.bourgault@gmail.com>
2020-09-15 13:47:02 -0400

    This fix makes me uncomfortable

--- xclim/core/formatting.py
+++ xclim/core/formatting.py
@@ -53,7 +53,7 @@ class AttrFormatter(string.Formatter):
 
         >>> fmt = AttrFormatter({'nice': ['beau', 'belle'], 'evil' : ['méchant', 'méchante']}, ['m', 'f'])
         >>> fmt.format("Le chien est {adj1:m}, l'oie est {adj2:f}", adj1='nice', adj2='evil')
-        'Le chien est beau, l'oie est méchante'
+        "Le chien est beau, l'oie est méchante"
 
         The base values may be given using unix shell-like patterns:
         >>> fmt = AttrFormatter({'AS-*': ['annuel', 'annuelle'], 'MS' : ['mensuel', 'mensuelle']}, ['m', 'f'])
