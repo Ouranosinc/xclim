64c8c5bb7788b4b0d1b8d0a6123c8f6a15478c69
Phobos <pascal.bourgault@gmail.com>
2020-11-10 18:28:38 -0500

    Tebaldi et al.

--- xclim/robustness.py
+++ xclim/robustness.py
@@ -4,55 +4,209 @@
 Robustness metrics
 ==================
 """
+from collections import namedtuple
+from typing import Mapping, Sequence, Union
+
 import numpy as np
+import scipy.stats as spstats
 import xarray as xr
+from boltons.funcutils import wraps
 from scipy.integrate import quad
 
 import xclim.indices.stats as xcstats
 
+metrics = {}
+norm = xcstats.get_dist("norm")
+Metric = namedtuple("Metric", ["func", "mapping", "dtype", "hist_dims"])
 
-def knutti_sedlacek(hist, sims):
-    """Compute the robustness measure R as described by Knutti and Sedláček (2013).
+
+def robustness(hist: xr.DataArray, sims: xr.DataArray, method: str, **params):
+    """Compute the robustness of an ensemble for a given indicator and change.
 
     Parameters
     ----------
     hist : xr.DataArray
-      Array with the historical values of the indicator along 'time'.
+      The indicator for the reference period. Shape and source depends on the metric.
     sims : xr.DataArray
-      Array with the simulated projected values of the indicators for different models ('ensembles') along 'time'.
+      The simulated projected values of the indicator (along `time`) for different models ('realization`).
+    methodc : {'knutti_sedlacek', 'tebaldi_et_al'}
+      The metric to compute.
+    **params:
+      Other metric-dependent parameters. See the method's docstring for more info.
 
     Returns
     -------
-    DataArray
-      The robustness metric R.
+    metric : DataArray
+      The robustness metric category as an integer (except for method `knutti_sedlacek` where a float is returned.).
+    mapping : dict or None
+      A mapping from values in `metric` to their signification. None for `knutti_sedlacek`.
     """
-    norm = xcstats.get_dist("norm")
+
+    try:
+        metric = metrics[method]
+    except KeyError:
+        raise ValueError(
+            f"Method {method} is not implemented. Available methods are : {','.join(metrics.keys())}."
+        )
 
     out = xr.apply_ufunc(
-        _knutti_sedlacek,
+        metric.func,
         hist,
         sims,
-        input_core_dims=[("realization",), ("realization", "time")],
+        input_core_dims=[metric.hist_dims, ("realization", "time")],
         vectorize=True,
         dask="parallelized",
-        output_dtypes=[float],
-        kwargs={"dist": norm},
+        output_dtypes=[metric.dtype],
+        kwargs=params,
     )
-    return out
+
+    out.name = "robustness"
+    out.attrs.update(
+        long_name=f"Ensemble robustness according to method {method}",
+        method=method,
+    )
+    return out, metric.mapping
 
 
-def _knutti_sedlacek(hist, sims, dist):
-    """Pointwise computation function."""
+def metric(
+    mapping: Mapping[int, str] = None,
+    na_value: Union[int, float] = 999,
+    hist_dims: Sequence[str] = ["realization", "time"],
+):
+    """Register a metric function in the `metrics` mapping and add some preparation/checking code.
 
-    def diff_cdf_area(x, l1, s1, l2, s2, dist):
-        return (dist.cdf(x, loc=l1, scale=s1) - dist.cdf(x, loc=l2, scale=s2)) ** 2
+    Parameters
+    ----------
+    mapping : Mapping[int, str], optional
+      A mapping from a robustness category to a short english description.
+    na_value : Union[int, float]
+      The value returned when any input value is NaN. It is added (if not present),
+      to the mapping with the description "Invalid values - no metric."
+    hist_dims : {['realization'], ['time'], ['realization', 'time']}
+      The dimension(s) expected on hist, used in the shape check.
 
-    l1, s1 = dist.fit(sims.flatten())
-    l2, s2 = dist.fit(sims.mean(axis=-1).flatten())
-    lh, sh = dist.fit(hist.flatten())
+    All metric functions are invalid when any non-finite values are present in the inputs.
+    """
 
-    int_bnds = dist.ppf(0.0001, loc=l1, scale=s1), dist.ppf(0.9999, loc=l1, scale=s1)
-    A1 = quad(diff_cdf_area, *int_bnds, args=(l1, s1, l2, s2, dist))[0]
-    A2 = quad(diff_cdf_area, *int_bnds, args=(lh, sh, l2, s2, dist))[0]
+    def _metric(func):
+        @wraps(func)
+        def _metric_overhead(hist, sims, **kwargs):
+            if np.any(np.isnan(hist)) or np.any(np.isnan(sims)):
+                return na_value
+
+            if (
+                (hist_dims == ["realization"] and hist.shape[0] != sims.shape[0])
+                or (hist_dims == ["time"] and hist.shape[0] != sims.shape[1])
+                or (hist_dims == ["realization", "time"] and hist.shape != sims.shape)
+            ):
+                raise AttributeError("Shape mismatch")
+
+            return func(hist, sims, **kwargs)
+
+        if mapping is not None:
+            mapping.setdefault(na_value, "Invalid values - no metric.")
+        metrics[func.__name__] = Metric(
+            _metric_overhead, mapping, type(na_value), hist_dims
+        )
+        return _metric_overhead
+
+    return _metric
+
+
+@metric(na_value=np.nan, hist_dims=["time"])
+def _knutti_sedlacek(hist, sims):
+    """Robustness metric from Knutti and Sedlacek (2013).
+
+    The robustness metric is defined as R = 1 − A1 / A2 , where A1 is defined
+    as the integral of the squared area between two cumulative density functions
+    characterizing the individual model projections and the multi-model mean
+    projection and A2 is the integral of the squared area between two cumulative
+    density functions characterizing the multi-model mean projection and the historical
+    climate. (Description taken from [knutti2013]_)
+
+    A value of R equal to one implies perfect model agreement. Higher model spread or
+    smaller signal decreases the value of R.
+
+    Parameters
+    ----------
+    hist : ndarray
+      1D Array, historical values along 'time'.
+    sims : ndarray
+      2D Array, simulated values along 'realization' and 'time'.
+
+    Returns
+    -------
+    R, float
+      The robustness metric.
+
+    References
+    ----------
+    .. [knnuti2013] Knutti, R. and Sedláček, J. (2013) Robustness and uncertainties in the new CMIP5 climate model projections. Nat. Clim. Change. doi:10.1038/nclimate1716
+    """
+
+    def diff_cdf_area(x, l1, s1, l2, s2, norm):
+        return (norm.cdf(x, loc=l1, scale=s1) - norm.cdf(x, loc=l2, scale=s2)) ** 2
+
+    l1, s1 = norm.fit(sims.flatten())
+    l2, s2 = norm.fit(sims.mean(axis=-1).flatten())
+    lh, sh = norm.fit(hist.flatten())
+
+    int_bnds = norm.ppf(0.0001, loc=l1, scale=s1), norm.ppf(0.9999, loc=l1, scale=s1)
+    A1 = quad(diff_cdf_area, *int_bnds, args=(l1, s1, l2, s2, norm))[0]
+    A2 = quad(diff_cdf_area, *int_bnds, args=(lh, sh, l2, s2, norm))[0]
 
     return 1 - A1 / A2
+
+
+@metric(
+    {
+        0: "No significant change",
+        1: "No agreement on sign of change",
+        2: "Agreement on sign of change",
+    },
+    hist_dims=["realization"],
+)
+def tebaldi_et_al(hist, sims, X=0.5, Y=0.8, p_change=0.05):
+    """Robustness categories from Tebaldi et al. (2011).
+
+    Compares the historical mean to the projected values for each model.
+    Category 0 is if less than X% of the models show significant change.
+    Category 1 is if models show significant change but less then Y% agree on its sign.
+    Category 2 is if models show significant change and agree on its sign.
+
+    Parameters
+    ----------
+    hists : ndarray
+      1D Array, multi-model historical mean along 'realization'.
+    sims : ndarray
+      2D Array, simulated values along 'realization' and 'time'.
+    X : float, 0 .. 1
+      Threshold fraction of models agreeing on significant change.
+    Y : float, 0 .. 1
+      Threshold fraction of models agreeing on the sign of the change.
+    p_change : float
+      p-value threshold for rejecting the hypothesis of no significant
+      change in the t-test.
+
+    Returns
+    -------
+    int
+      0 for no significant change, 1 for change but no agreement on sign of change
+      and 2 for change and agreement on sign.
+
+    References
+    ----------
+    Tebaldi C., Arblaster, J.M. and Knutti, R. (2011) Mapping model agreement on future climate projections. GRL. doi:10.1029/2011GL049863
+    """
+    # Test hypothesis of no significant change
+    t, p = spstats.ttest_1samp(sims, hist, axis=1)
+    # When p < p_change, the hypothesis of no significant change is rejected.
+    # We need at least X % models showing significant change
+    if (p < p_change).sum() / hist.size < X:
+        return 0  # No significant change
+
+    # Test that models agree on the sign of the change
+    change_sign = np.sign(sims.mean(axis=1) - hist)
+    if (1 - Y) < change_sign.sum() / hist.size < Y:
+        return 1  # Significant change but no agreement on sign of change
+    return 2  # Significant change and agreement on sign of change
