6700cc11636803249770771030728a9905cb8128
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-04-26 13:38:00 -0400

    More docstring changes and some type ignores

--- xclim/indicators/atmos/_temperature.py
+++ xclim/indicators/atmos/_temperature.py
@@ -662,7 +662,8 @@ degree_days_exceedance_date = Tas(
 
 warm_spell_duration_index = Tasmax(
     identifier="warm_spell_duration_index",
-    description="{freq} total number of days within spells of at least {window} days with tmax above the 90th daily percentile.",
+    description="{freq} total number of days within spells of at least {window} days"
+    " with tmax above the 90th daily percentile.",
     units="days",
     standard_name="number_of_days_with_air_temperature_above_threshold",
     cell_methods="time: sum over days",
