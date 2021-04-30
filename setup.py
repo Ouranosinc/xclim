1c50095c548634cfe72fb852550330c063f5f146
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2021-04-30 18:06:56 -0400

    Update required Python to >=3.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ DESCRIPTION = "Derived climate variables built with xarray."
 URL = "https://github.com/Ouranosinc/xclim"
 AUTHOR = "Travis Logan"
 AUTHOR_EMAIL = "logan.travis@ouranos.ca"
-REQUIRES_PYTHON = ">=3.6.0"
+REQUIRES_PYTHON = ">=3.7.0"
 VERSION = "0.26.0"
 LICENSE = "Apache Software License 2.0"
 
