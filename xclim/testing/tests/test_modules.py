85790c006ac07cfed8082e8e8d9f5509fe426fbb
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2021-06-24 13:28:37 -0400

    Refactor and fix test_modules

--- xclim/testing/tests/test_modules.py
+++ xclim/testing/tests/test_modules.py
@@ -4,10 +4,22 @@ from pathlib import Path
 import pytest
 import xarray as xr
 
-from xclim.core.indicator import build_indicator_module_from_yaml, registry
+from xclim import indicators
+from xclim.core.indicator import build_indicator_module_from_yaml
 from xclim.core.options import set_options
 from xclim.core.utils import InputKind
 from xclim.testing import open_dataset
+
+
+def all_virtual_indicators():
+    for mod in ["anuclim", "cf", "icclim"]:
+        for name, ind in getattr(indicators, mod).iter_indicators():
+            yield pytest.param((mod, name, ind), id=f"{mod}.{name}")
+
+
+@pytest.fixture(params=all_virtual_indicators())
+def virtual_indicator(request):
+    return request.param
 
 
 def test_default_modules_exist():
@@ -26,52 +38,21 @@ def test_default_modules_exist():
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize(
-    "indname", [name for name in registry.keys() if name.startswith("cf.")]
-)
-def test_cf(indname, atmosds):
+def test_virtual_modules(virtual_indicator, atmosds):
     with set_options(cf_compliance="warn"):
         # skip when missing default values
-        ind = registry[indname].get_instance()
+        kws = {}
+        mod, indname, ind = virtual_indicator
         for name, param in ind.parameters.items():
-            if param["kind"] is not InputKind.DATASET and param["default"] in (
-                None,
-                name,
+            if name == "src_timestep":
+                kws["src_timestep"] = "D"
+            if param["kind"] is not InputKind.DATASET and (
+                param["default"] is None
+                or (param["default"] == name and name not in atmosds)
             ):
-                pytest.skip(f"Indicator {ind.identifier} has no default for {name}.")
+
+                pytest.skip(f"Indicator {mod}.{indname} has no default for {name}.")
         ind(ds=atmosds)
-
-
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    "indname", [name for name in registry.keys() if name.startswith("icclim.")]
-)
-def test_icclim(indname, atmosds):
-    # skip when missing default values
-    ind = registry[indname].get_instance()
-    for name, param in ind.parameters.items():
-        if param["kind"] is not InputKind.DATASET and param["default"] in (None, name):
-            pytest.skip(f"Indicator {ind.identifier} has no default for {name}.")
-    ind(ds=atmosds)
-
-
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    "indname", [name for name in registry.keys() if name.startswith("anuclim.")]
-)
-def test_anuclim(indname, atmosds):
-    # skip when missing default values
-    ind = registry[indname].get_instance()
-    kws = {}
-    for name, param in ind.parameters.items():
-        if name == "src_timestep":
-            kws["src_timestep"] = "D"
-        elif param["kind"] is not InputKind.DATASET and param["default"] in (
-            None,
-            name,
-        ):
-            pytest.skip(f"Indicator {ind.identifier} has no default for {name}.")
-    ind(ds=atmosds, **kws)
 
 
 def test_custom_indices():
