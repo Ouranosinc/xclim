66b17afc91b6c879740088a0e77101e97fe28a1e
abel <aoun@cerfacs.fr>
2021-09-16 18:09:53 +0200

    Add tests for dask

--- xclim/testing/tests/test_bootstrapping.py
+++ xclim/testing/tests/test_bootstrapping.py
@@ -21,78 +21,109 @@ from xclim.testing import open_dataset
 
 
 class Test_bootstrap:
-    @pytest.mark.slow
-    def test_bootstrap_tg90p(self, tas_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_tg90p(self, tas_series, use_dask):
         self.bootstrap_testor(
-            tas_series, 98, lambda x, y, z: tg90p(x, y, freq="MS", bootstrap=z)
+            tas_series,
+            98,
+            lambda x, y, z: tg90p(x, y, freq="MS", bootstrap=z),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_tn90p(self, tasmin_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_tn90p(self, tasmin_series, use_dask):
         self.bootstrap_testor(
-            tasmin_series, 98, lambda x, y, z: tn90p(x, y, freq="A-JUL", bootstrap=z)
+            tasmin_series,
+            98,
+            lambda x, y, z: tn90p(x, y, freq="A-JUL", bootstrap=z),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_tx90p(self, tasmax_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_tx90p(self, tasmax_series, use_dask):
         self.bootstrap_testor(
-            tasmax_series, 98, lambda x, y, z: tx90p(x, y, freq="Q-APR", bootstrap=z)
+            tasmax_series,
+            98,
+            lambda x, y, z: tx90p(x, y, freq="Q-APR", bootstrap=z),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_tn10p(self, tasmin_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_tn10p(self, tasmin_series, use_dask):
         self.bootstrap_testor(
-            tasmin_series, 2, lambda x, y, z: tn10p(x, y, freq="MS", bootstrap=z)
+            tasmin_series,
+            2,
+            lambda x, y, z: tn10p(x, y, freq="MS", bootstrap=z),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_tx10p(self, tasmax_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_tx10p(self, tasmax_series, use_dask):
         self.bootstrap_testor(
-            tasmax_series, 2, lambda x, y, z: tx10p(x, y, freq="YS", bootstrap=z)
+            tasmax_series,
+            2,
+            lambda x, y, z: tx10p(x, y, freq="YS", bootstrap=z),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_tg10p(self, tas_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_tg10p(self, tas_series, use_dask):
         self.bootstrap_testor(
-            tas_series, 2, lambda x, y, z: tg10p(x, y, freq="MS", bootstrap=z)
+            tas_series,
+            2,
+            lambda x, y, z: tg10p(x, y, freq="MS", bootstrap=z),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_warm_spell_duration_index(self, tasmax_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_warm_spell_duration_index(self, tasmax_series, use_dask):
         self.bootstrap_testor(
             tasmax_series,
             98,
             lambda x, y, z: warm_spell_duration_index(
                 x, y, window=6, freq="MS", bootstrap=z
             ),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_cold_spell_duration_index(self, tasmin_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_cold_spell_duration_index(self, tasmin_series, use_dask):
         self.bootstrap_testor(
             tasmin_series,
             2,
             lambda x, y, z: cold_spell_duration_index(
                 x, y, window=6, freq="MS", bootstrap=z
             ),
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_days_over_precip_thresh(self, pr_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_days_over_precip_thresh(self, pr_series, use_dask):
         self.bootstrap_testor(
             pr_series,
             99,
             lambda x, y, z: days_over_precip_thresh(x, y, freq="MS", bootstrap=z),
             positive_values=True,
+            use_dask=use_dask,
         )
 
     @pytest.mark.slow
-    def test_bootstrap_fraction_over_precip_thresh(self, pr_series):
+    @pytest.mark.parametrize("use_dask", [True, False])
+    def test_bootstrap_fraction_over_precip_thresh(self, pr_series, use_dask):
         self.bootstrap_testor(
             pr_series,
             98,
             lambda x, y, z: fraction_over_precip_thresh(x, y, freq="MS", bootstrap=z),
             positive_values=True,
+            use_dask=use_dask,
         )
 
     def bootstrap_testor(
@@ -101,11 +132,15 @@ class Test_bootstrap:
         per: int,
         compute_indice: Callable[[DataArray, DataArray], DataArray],
         positive_values=False,
+        use_dask=False,
     ):
         # GIVEN
         n = int(4 * 365.25)
         alpha = 0.8
-        climat_variable = serie(self.ar1(alpha, n, positive_values), start="2000-01-01")
+        arr = self.ar1(alpha, n, positive_values)
+        climat_variable = serie(arr, start="2000-01-01")
+        if use_dask:
+            climat_variable = climat_variable.chunk(dict(time=2))
         in_base_slice = slice("2000-01-01", "2001-12-31")
         out_base_slice = slice("2002-01-01", "2003-12-31")
         per = percentile_doy(climat_variable.sel(time=in_base_slice), per=per)
