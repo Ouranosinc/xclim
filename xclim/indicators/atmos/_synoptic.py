651fc3c0610f6e5490a744a12c748406654348cd
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-12-17 12:28:12 +0000

    format file

--- xclim/indicators/atmos/_synoptic.py
+++ xclim/indicators/atmos/_synoptic.py
@@ -9,14 +9,20 @@ __all__ = ["jetstream_metric_woolings"]
 
 class JetStream(Indicator):
     """Indicator involving daily u- and/or v-component wind series."""
-    
-    src_freq = 'D'
+
+    src_freq = "D"
 
 
 jetstream_metric_woolings = JetStream(
     identifier="jetstream_metric_woolings",
     units=["degrees_North", "m s-1"],
-    long_name=["Latitude of maximum smoothed zonal wind speed", "Maximum strength of smoothed zonal wind speed"],
-    description=["Daily latitude of maximum smoothed zonal wind speed", "Daily maximum strength of smoothed zonal wind speed"],
+    long_name=[
+        "Latitude of maximum smoothed zonal wind speed",
+        "Maximum strength of smoothed zonal wind speed",
+    ],
+    description=[
+        "Daily latitude of maximum smoothed zonal wind speed",
+        "Daily maximum strength of smoothed zonal wind speed",
+    ],
     compute=indices.jetstream_metric_woolings,
 )
