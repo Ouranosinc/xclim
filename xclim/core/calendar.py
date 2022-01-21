6e5df24ce5a81d6cf370e4aaf6ce2f9a26661db8
abel <aoun@cerfacs.fr>
2022-01-21 11:08:54 +0100

    ENH: Update percentile chunking
    Currently, percentile_doy multiply the chunk size by at
    least 5 (window width) but it can grow even bigger because the time
    dimension initial chunking is not used in the rechunk.
    This is fixed by rechunking the new dayofyear dimension
    at in 5 chunks but, ideally in (window * initial_time_chunk_count) chunks.
    
    Related to gh #932

--- xclim/core/calendar.py
+++ xclim/core/calendar.py
@@ -491,7 +491,10 @@ def percentile_doy(
     rrr = rr.assign_coords(time=ind).unstack("time").stack(stack_dim=("year", "window"))
 
     if rrr.chunks is not None and len(rrr.chunks[rrr.get_axis_num("stack_dim")]) > 1:
-        rrr = rrr.chunk(dict(stack_dim=-1))
+        # Preserve chunk size
+        time_chunks_count = len(arr.chunks[arr.get_axis_num("time")])
+        doy_chunk_size = round(len(rrr.dayofyear) / (window * time_chunks_count))
+        rrr = rrr.chunk(dict(stack_dim=-1, dayofyear=doy_chunk_size))
 
     if np.isscalar(per):
         per = [per]
