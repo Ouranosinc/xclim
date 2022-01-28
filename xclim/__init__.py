a2c9c78bae52ffa146d0754a52b98b1dbe5883c8
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2022-01-28 16:11:49 -0500

    Bump version: 0.33.0-beta → 0.33.0

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -10,7 +10,7 @@ from xclim.indicators import atmos, land, seaIce  # noqa
 
 __author__ = """Travis Logan"""
 __email__ = "logan.travis@ouranos.ca"
-__version__ = "0.33.0-beta"
+__version__ = "0.33.0"
 
 
 # Load official locales
