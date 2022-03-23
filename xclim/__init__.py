8480f3ed508487ccc49dba8b1b4f15680d7d86a6
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2022-03-23 16:35:18 -0400

    Add loguru standard configuration to xclim

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -1,4 +1,6 @@
 """Climate indices computation package based on Xarray."""
+import logging
+import sys
 from importlib.resources import contents, path
 
 from loguru import logger
@@ -13,7 +15,38 @@ __author__ = """Travis Logan"""
 __email__ = "logan.travis@ouranos.ca"
 __version__ = "0.34.1-beta"
 
+
+# On import, clisops sets a root logger for console output. This is huge annoyance to deal with.
+# For upstream fix discussion, see: https://github.com/roocs/clisops/pull/216
+if "clisops" in sys.modules:
+    root_logger = logging.getLogger()
+    root_logger.removeHandler(root_logger.handlers[0])
+
+
+# Gather logged events from standard logging
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+# Send logged events to loguru handler and deactivate it
+logging.basicConfig(handlers=[InterceptHandler()], level=0)
 logger.disable("xclim")
+
 
 # Load official locales
 for filename in contents("xclim.data"):
