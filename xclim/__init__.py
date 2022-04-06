eee3106260940e39a97ab113f41b16bf7ff37f39
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2022-04-06 14:39:54 -0400

    Bump version: 0.35.0 → 0.35.1-beta

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -9,7 +9,7 @@ from xclim.indicators import atmos, land, seaIce  # noqa
 
 __author__ = """Travis Logan"""
 __email__ = "logan.travis@ouranos.ca"
-__version__ = "0.35.0"
+__version__ = "0.35.1-beta"
 
 
 # Load official locales
