5ee8a447170dd57546c5e01334e29886677632ca
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2022-04-26 12:05:46 +0200

    UTCI first try"

--- xclim/indices/_multivariate.py
+++ xclim/indices/_multivariate.py
@@ -58,6 +58,7 @@ __all__ = [
     "tx_tn_days_above",
     "warm_spell_duration_index",
     "winter_rain_ratio",
+    "universal_thermal_climate_index",
 ]
 
 
@@ -1736,3 +1737,51 @@ def blowing_snow(
     out = cond.resample(time=freq).sum(dim="time")
     out.attrs["units"] = to_agg_units(out, snd, "count")
     return out
+
+def xutci(tdb, tr, v, rh, freq):
+    from pythermalcomfort.models import utci
+    idx = xarray.apply_ufunc(
+        utci,
+        tdb,
+        tr,
+        v,
+        rh).assign_attrs({"units":"degC"})
+    return idx.resample(time=freq)
+
+@declare_units(
+    tas="[temperature]", hurs="[]", sfcWind="[speed]" 
+)
+def universal_thermal_climate_index(
+    tas: xarray.DataArray,
+    hurs: xarray.DataArray,
+    sfcWind: xarray.DataArray,
+    freq: str = 'YS',
+) -> xarray.DataArray:
+    """
+    Mean Universal Thermal Climate Index (UTCI)
+
+    The mean of daily UTCI
+    
+    Parameters
+    ----------
+    tas : xarray.DataArray
+        Mean daily temperature
+    hurs : xarray.DataArray
+        Relative Humidity
+    sfcWind : xarray.DataArray
+        Wind velocity
+    freq : str
+        Resampling frequency.
+
+    Returns
+    -------
+    xarray.DataArray
+        Ultimate Thermal Climate Index.
+    """
+    
+    tas = convert_units_to(tas, "degC")
+    hurs = convert_units_to(hurs, "pct")
+
+    arr = xutci(tdb=tas, tr=tas, v=sfcWind, rh=hurs, freq=freq)
+    return arr.mean(dim="time", keep_attrs=True)
+
