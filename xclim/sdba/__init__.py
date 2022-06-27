17874680a2bae96dda199194d47ae7c9ca0a8523
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2022-06-27 15:02:48 -0400

    Apply suggestions from code review
    Co-authored-by: David Huard <huard.david@ouranos.ca>

--- xclim/sdba/__init__.py
+++ xclim/sdba/__init__.py
@@ -96,6 +96,7 @@ from .processing import (
 # TODO: ISIMIP ? Used for precip freq adjustment in biasCorrection.R
 # Hempel, S., Frieler, K., Warszawski, L., Schewe, J., & Piontek, F. (2013). A trend-preserving bias correction &ndash;
 # The ISI-MIP approach. Earth System Dynamics, 4(2), 219–236. https://doi.org/10.5194/esd-4-219-2013
+# If SBCK is installed, create adjustment classes wrapping SBCK's algorithms.
 if hasattr(adjustment, "_generate_SBCK_classes"):
     for cls in adjustment._generate_SBCK_classes():
         adjustment.__dict__[cls.__name__] = cls
