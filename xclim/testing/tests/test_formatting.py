31f26ba0d5cb085ee4b560d9e106f1bf8622a47e
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-08-12 14:17:49 -0400

    typo fixes

--- xclim/testing/tests/test_formatting.py
+++ xclim/testing/tests/test_formatting.py
@@ -25,7 +25,7 @@ def test_prefix_attrs():
 
 def test_indicator_docstring():
     doc = heat_wave_frequency.__doc__.split("\n")
-    assert doc[0] == "Heat wave frequency. (realm: atmos)"
+    assert doc[0] == "Heat wave frequency (realm: atmos)"
     assert (
         doc[5]
         == "Based on indice :py:func:`~xclim.indices._multivariate.heat_wave_frequency`."
