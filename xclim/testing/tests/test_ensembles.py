680abdc3a4ced39dbcbc7c5be424a5b21c2dcbf6
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2022-08-23 16:38:26 -0400

    fix tests

--- xclim/testing/tests/test_ensembles.py
+++ xclim/testing/tests/test_ensembles.py
@@ -252,7 +252,7 @@ class TestEnsembleStats:
         out2 = ensembles.ensemble_mean_std_max_min(ens, weights=weights)
         values = ens["tg_mean"][:, 0, 5, 5]
         np.testing.assert_array_equal(
-            (values[0]*10 + values[1]*1 + values[2]*15 + values[3]*5) / 31,
+            (values[0]*1 + values[1]*.1 + values[2]*3.5 + values[3]*5) / np.sum(weights),
             out2.tg_mean_mean[0, 5, 5],
         )
         np.testing.assert_array_equal(
