6e9366524f1da1156c0bcbaa48f18f776bc1dcf9
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2023-07-27 10:26:03 -0400

    Forgot some test tweaks

--- tests/test_ensembles.py
+++ tests/test_ensembles.py
@@ -605,7 +605,7 @@ def robust_data(request, random):
             [0.75, 1, 1, 1],
             [2 / 3, 0.5, 1, 1],
             [
-                [False, False, False, True],
+                [False, True, True, True],
                 [True, True, True, True],
                 [True, True, True, True],
                 [False, False, True, True],
@@ -615,7 +615,7 @@ def robust_data(request, random):
         (
             "welch-ttest",
             [0.25, 1, 1, 1],
-            [0.5, 0.5, 1, 1],
+            [1, 0.5, 1, 1],
             [
                 [False, False, False, True],
                 [True, True, True, True],
@@ -627,9 +627,9 @@ def robust_data(request, random):
         (
             "mannwhitney-utest",
             [0.5, 1, 1, 1],
-            [1, 0.5, 1, 1],
+            [0.5, 0.5, 1, 1],
             [
-                [False, False, False, True],
+                [False, False, True, True],
                 [True, True, True, True],
                 [True, True, True, True],
                 [False, False, True, True],
@@ -641,10 +641,10 @@ def robust_data(request, random):
             [0.25, 0.25, 0.25, 0],
             [1, 0.0, 1, np.nan],
             [
-                [False, True, False, True],
+                [False, True, False, False],
                 [True, False, False, False],
-                [False, True, False, True],
                 [False, False, False, True],
+                [False, False, False, False],
             ],
             {},
         ),
