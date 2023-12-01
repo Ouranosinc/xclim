a3e137d1e88092f58ba9150a39b341376eac8824
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-12-01 13:00:47 -0500

    Bump version: 0.47.0-beta → 0.47.0

--- xclim/__init__.py
+++ xclim/__init__.py
@@ -15,7 +15,7 @@ from xclim.indicators import atmos, generic, land, seaIce  # noqa
 
 __author__ = """Travis Logan"""
 __email__ = "logan.travis@ouranos.ca"
-__version__ = "0.47.0-beta"
+__version__ = "0.47.0"
 
 
 _module_data = _files("xclim.data")
