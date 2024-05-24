7c5463a8b9d5a52ab78535de69494fe54727d4fe
juliettelavoie <juliette.lavoie@hotmail.ca>
2024-05-24 09:13:31 -0400

    init

--- xclim/ensembles/__init__.py
+++ xclim/ensembles/__init__.py
@@ -11,7 +11,12 @@ or models are concatenated along the `realization` dimension.
 from __future__ import annotations
 
 from ._base import create_ensemble, ensemble_mean_std_max_min, ensemble_percentiles
-from ._partitioning import fractional_uncertainty, hawkins_sutton, lafferty_sriver
+from ._partitioning import (
+    fractional_uncertainty,
+    general_partition,
+    hawkins_sutton,
+    lafferty_sriver,
+)
 from ._reduce import (
     kkz_reduce_ensemble,
     kmeans_reduce_ensemble,
