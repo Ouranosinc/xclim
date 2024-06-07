3205f657779b21b46244f64b7d02986056aad34e
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2024-06-07 10:56:33 -0400

    fix hawkins-sutton smoke test

--- xclim/ensembles/_filters.py
+++ xclim/ensembles/_filters.py
@@ -4,20 +4,20 @@ import numpy as np
 import xarray as xr
 
 
-def _concat_hist(da, **hist):
-    """Concatenate historical scenario with future scenarios along time.
+def _concat_hist(da: xr.DataArray, **hist) -> xr.DataArray:
+    r"""Concatenate historical scenario with future scenarios along the time dimension.
 
     Parameters
     ----------
     da : xr.DataArray
-      Input data where the historical scenario is stored alongside other, future, scenarios.
-    hist: {str: str}
-      Mapping of the scenario dimension name to the historical scenario coordinate, e.g. `scenario="historical"`.
+        Input data where the historical scenario is stored alongside other, future, scenarios.
+    \*\*hist : dict
+        Mapping of the scenario dimension name to the historical scenario coordinate, e.g. `scenario="historical"`.
 
     Returns
     -------
     xr.DataArray
-      Data with the historical scenario is stacked in time before each one of the other scenarios.
+        Data with the historical scenario is stacked in time before each one of the other scenarios.
 
     Notes
     -----
@@ -51,6 +51,7 @@ def _concat_hist(da, **hist):
 
     # Select historical scenario and drop it from the data
     h = da.sel(**hist).dropna("time", how="all")
+    h = h.drop_vars(dim)
     ens = da.drop_sel(**hist)
 
     index = ens[dim]
@@ -59,15 +60,17 @@ def _concat_hist(da, **hist):
     return xr.concat([h, bare], dim="time").assign_coords({dim: index})
 
 
-def _model_in_all_scens(da, dimensions=None):
+def _model_in_all_scens(
+    da: xr.DataArray, dimensions: dict | None = None
+) -> xr.DataArray:
     """Return data with only simulations that have at least one member in each scenario.
 
     Parameters
     ----------
-    da: xr.DataArray
-      Input data with dimensions for time, member, model and scenario.
-    dimensions: dict
-      Mapping from original dimension names to standard dimension names: scenario, model, member.
+    da : xr.DataArray
+        Input data with dimensions for time, member, model and scenario.
+    dimensions : dict, optional
+        Mapping from original dimension names to standard dimension names: scenario, model, member.
 
     Returns
     -------
@@ -100,20 +103,20 @@ def _model_in_all_scens(da, dimensions=None):
     return da.sel(model=ok).rename(dimensions)
 
 
-def _single_member(da, dimensions=None):
+def _single_member(da: xr.DataArray, dimensions: dict | None = None) -> xr.DataArray:
     """Return data for a single member per model.
 
     Parameters
     ----------
     da : xr.DataArray
-      Input data with dimensions for time, member, model and scenario.
-    dimensions: dict
-      Mapping from original dimension names to standard dimension names: scenario, model, member.
+        Input data with dimensions for time, member, model and scenario.
+    dimensions : dict
+        Mapping from original dimension names to standard dimension names: scenario, model, member.
 
     Returns
     -------
     xr.DataArray
-      Data with only one member per model.
+        Data with only one member per model.
 
     Notes
     -----
@@ -147,6 +150,6 @@ def _single_member(da, dimensions=None):
     return out.rename(dimensions)
 
 
-def reverse_dict(d):
+def reverse_dict(d: dict) -> dict:
     """Reverse dictionary."""
     return {v: k for (k, v) in d.items()}
