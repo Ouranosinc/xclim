fix standardize method and 'transform' -> 'normalization'

xclim/sdba/_adjustment.py
@@ -953,7 +953,7 @@ def _otc_adjust(
     bin_origin: dict | float | np.ndarray | None = None,
     num_iter_max: int | None = 100_000_000,
     jitter_inside_bins: bool = True,
-    transform: str | None = "max_distance",
+    normalization: str | None = "max_distance",
 ):
     """Optimal Transport Correction of the bias of X with respect to Y.
 
@@ -972,8 +972,9 @@ def _otc_adjust(
     jitter_inside_bins : bool
         If `False`, output points are located at the center of their bin.
         If `True`, a random location is picked uniformly inside their bin. Default is `True`.
-    transform : {None, 'standardize', 'max_distance', 'max_value'}
-        Per-variable transformation applied before the distances are calculated.
+    normalization : {None, 'standardize', 'max_distance', 'max_value'}
+        Per-variable transformation applied before the distances are calculated
+        in the optimal transport.
 
     Returns
     -------
@@ -1013,7 +1014,7 @@ def _otc_adjust(
     gridY, muY, _ = u.histogram(Y, bin_width, bin_origin)
 
     # Compute the optimal transportation plan
-    plan = u.optimal_transport(gridX, gridY, muX, muY, num_iter_max, transform)
+    plan = u.optimal_transport(gridX, gridY, muX, muY, num_iter_max, normalization)
 
     gridX = np.floor((gridX - bin_origin) / bin_width)
     gridY = np.floor((gridY - bin_origin) / bin_width)
@@ -1051,7 +1052,7 @@ def otc_adjust(
     num_iter_max: int | None = 100_000_000,
     jitter_inside_bins: bool = True,
     adapt_freq_thresh: dict | None = None,
-    transform: str | None = "max_distance",
+    normalization: str | None = "max_distance",
 ):
     """Optimal Transport Correction of the bias of `hist` with respect to `ref`.
 
@@ -1076,8 +1077,9 @@ def otc_adjust(
         If `True`, a random location is picked uniformly inside their bin. Default is `True`.
     adapt_freq_thresh : dict, optional
         Threshold for frequency adaptation per variable.
-    transform : {None, 'standardize', 'max_distance', 'max_value'}
-        Per-variable transformation applied before the distances are calculated.
+    normalization : {None, 'standardize', 'max_distance', 'max_value'}
+        Per-variable transformation applied before the distances are calculated
+        in the optimal transport.
 
     Returns
     -------
@@ -1121,7 +1123,7 @@ def otc_adjust(
             bin_origin=bin_origin,
             num_iter_max=num_iter_max,
             jitter_inside_bins=jitter_inside_bins,
-            transform=transform,
+            normalization=normalization,
         ),
         input_core_dims=[["dim_hist", pts_dim], ["dim_ref", pts_dim]],
         output_core_dims=[["dim_hist", pts_dim]],
@@ -1149,7 +1151,7 @@ def _dotc_adjust(
     cov_factor: str | None = "std",
     jitter_inside_bins: bool = True,
     kind: dict | None = None,
-    transform: str | None = "max_distance",
+    normalization: str | None = "max_distance",
 ):
     """Dynamical Optimal Transport Correction of the bias of X with respect to Y.
 
@@ -1175,8 +1177,9 @@ def _dotc_adjust(
     kind : dict, optional
         Keys are variable names and values are adjustment kinds, either additive or multiplicative.
         Unspecified dimensions are treated as "+".
-    transform : {None, 'standardize', 'max_distance', 'max_value'}
-        Per-variable transformation applied before the distances are calculated.
+    normalization : {None, 'standardize', 'max_distance', 'max_value'}
+        Per-variable transformation applied before the distances are calculated
+        in the optimal transport.
 
     Returns
     -------
@@ -1212,7 +1215,7 @@ def _dotc_adjust(
         bin_origin=bin_origin,
         num_iter_max=num_iter_max,
         jitter_inside_bins=False,
-        transform=transform,
+        normalization=normalization,
     )
 
     # Map hist to sim
@@ -1223,7 +1226,7 @@ def _dotc_adjust(
         bin_origin=bin_origin,
         num_iter_max=num_iter_max,
         jitter_inside_bins=False,
-        transform=transform,
+        normalization=normalization,
     )
 
     # Temporal evolution
@@ -1260,7 +1263,7 @@ def _dotc_adjust(
         bin_origin=bin_origin,
         num_iter_max=num_iter_max,
         jitter_inside_bins=jitter_inside_bins,
-        transform=transform,
+        normalization=normalization,
     )
 
     return Z1
@@ -1278,7 +1281,7 @@ def dotc_adjust(
     jitter_inside_bins: bool = True,
     kind: dict | None = None,
     adapt_freq_thresh: dict | None = None,
-    transform: str | None = "max_distance",
+    normalization: str | None = "max_distance",
 ):
     """Dynamical Optimal Transport Correction of the bias of X with respect to Y.
 
@@ -1309,8 +1312,9 @@ def dotc_adjust(
         Unspecified dimensions are treated as "+".
     adapt_freq_thresh : dict, optional
         Threshold for frequency adaptation per variable.
-    transform : {None, 'standardize', 'max_distance', 'max_value'}
-        Per-variable transformation applied before the distances are calculated.
+    normalization : {None, 'standardize', 'max_distance', 'max_value'}
+        Per-variable transformation applied before the distances are calculated
+        in the optimal transport.
 
     Returns
     -------
@@ -1368,7 +1372,7 @@ def dotc_adjust(
             cov_factor=cov_factor,
             jitter_inside_bins=jitter_inside_bins,
             kind=kind,
-            transform=transform,
+            normalization=normalization,
         ),
         input_core_dims=[
             ["dim_sim", pts_dim],

xclim/sdba/utils.py
@@ -1018,7 +1018,7 @@ def histogram(data, bin_width, bin_origin):
     return grid, mu, idx_bin
 
 
-def optimal_transport(gridX, gridY, muX, muY, numItermax, transform):
+def optimal_transport(gridX, gridY, muX, muY, num_iter_max, normalization):
     """Computes the optimal transportation plan on (transformations of) X and Y.
 
     References
@@ -1027,18 +1027,18 @@ def optimal_transport(gridX, gridY, muX, muY, numItermax, transform):
     """
     from ot import emd
 
-    if transform == "standardize":
-        gridX = (gridX - gridX.mean()) / gridX.std()
-        gridY = (gridY - gridY.mean()) / gridY.std()
+    if normalization == "standardize":
+        gridX = (gridX - gridX.mean(axis=0)) / gridX.std(axis=0)
+        gridY = (gridY - gridY.mean(axis=0)) / gridY.std(axis=0)
 
-    elif transform == "max_distance":
+    elif normalization == "max_distance":
         max1 = np.abs(gridX.max(axis=0) - gridY.min(axis=0))
         max2 = np.abs(gridY.max(axis=0) - gridX.min(axis=0))
         max_dist = np.maximum(max1, max2)
         gridX = gridX / max_dist
         gridY = gridY / max_dist
 
-    elif transform == "max_value":
+    elif normalization == "max_value":
         max_value = np.maximum(gridX.max(axis=0), gridY.max(axis=0))
         gridX = gridX / max_value
         gridY = gridY / max_value
@@ -1047,7 +1047,7 @@ def optimal_transport(gridX, gridY, muX, muY, numItermax, transform):
     C = distance.cdist(gridX, gridY, "sqeuclidean")
 
     # Compute the optimal transportation plan
-    gamma = emd(muX, muY, C, numItermax=numItermax)
+    gamma = emd(muX, muY, C, numItermax=num_iter_max)
     plan = (gamma.T / gamma.sum(axis=1)).T
 
     return plan