1bea7b9201d14be7a632bc05f25da81bd5f42a0a
kobebryant432 <kobe.ande@gmail.com>
2025-04-18 15:08:37 +0200

    remove overload

--- src/xclim/core/units.py
+++ src/xclim/core/units.py
@@ -14,7 +14,7 @@ from collections.abc import Callable
 from copy import deepcopy
 from importlib.resources import files
 from inspect import signature
-from typing import Any, Literal, cast, overload
+from typing import Any, Literal, cast
 
 import cf_xarray.units
 import numpy as np
