82074620cabe13ad4378753cd29d22f725461c74
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2025-05-21 14:56:45 -0400

    group tests more

--- tests/test_wind.py
+++ tests/test_wind.py
@@ -7,8 +7,10 @@ from xclim import atmos
 
 
 class TestWindSpeedIndicators:
+    test_data = "ERA5/daily_surface_cancities_1990-1993.nc"
+
     def test_calm_windy_days(self, nimbus):
-        ds = xr.open_dataset(nimbus.fetch("ERA5/daily_surface_cancities_1990-1993.nc"), engine="h5netcdf")
+        ds = xr.open_dataset(nimbus.fetch(self.test_data), engine="h5netcdf")
         sfcwind, _ = atmos.wind_speed_from_vector(ds.uas, ds.vas, calm_wind_thresh="0 m/s")
 
         calm = atmos.calm_days(sfcwind, thresh="5 m/s")
@@ -17,29 +19,27 @@ class TestWindSpeedIndicators:
         np.testing.assert_array_equal(calm + windy, c)
 
 
-class TestSfcWindMax:
+class TestSfcWind:
+    test_data = "ERA5/daily_surface_cancities_1990-1993.nc"
+
     def test_sfcWind_max(self, nimbus):
-        ds = xr.open_dataset(nimbus.fetch("ERA5/daily_surface_cancities_1990-1993.nc"), engine="h5netcdf")
+        ds = xr.open_dataset(nimbus.fetch(self.test_data), engine="h5netcdf")
         sfcWind, _ = atmos.wind_speed_from_vector(ds.uas, ds.vas)
 
         sfcWind_max = atmos.sfcWind_max(sfcWind)
         c = sfcWind.resample(time="YS").max()
         np.testing.assert_array_equal(sfcWind_max, c)
 
-
-class TestSfcWindMean:
     def test_sfcWind_mean(self, nimbus):
-        ds = xr.open_dataset(nimbus.fetch("ERA5/daily_surface_cancities_1990-1993.nc"), engine="h5netcdf")
+        ds = xr.open_dataset(nimbus.fetch(self.test_data), engine="h5netcdf")
         sfcWind, _ = atmos.wind_speed_from_vector(ds.uas, ds.vas)
 
         sfcWind_mean = atmos.sfcWind_mean(sfcWind)
         c = sfcWind.resample(time="YS").mean()
         np.testing.assert_array_equal(sfcWind_mean, c)
 
-
-class TestSfcWindMin:
     def test_sfcWind_min(self, nimbus):
-        ds = xr.open_dataset(nimbus.fetch("ERA5/daily_surface_cancities_1990-1993.nc"), engine="h5netcdf")
+        ds = xr.open_dataset(nimbus.fetch(self.test_data), engine="h5netcdf")
         sfcWind, _ = atmos.wind_speed_from_vector(ds.uas, ds.vas)
 
         sfcWind_min = atmos.sfcWind_min(sfcWind)
@@ -47,29 +47,27 @@ class TestSfcWindMin:
         np.testing.assert_array_equal(sfcWind_min, c)
 
 
-class TestSfcWindmaxMax:
+class TestSfcWindMax:
+    test_data = "ERA5/daily_surface_cancities_1990-1993.nc"
+
     def test_sfcWindmax_max(self, nimbus):
-        ds = xr.open_dataset(nimbus.fetch("ERA5/daily_surface_cancities_1990-1993.nc"), engine="h5netcdf")
+        ds = xr.open_dataset(nimbus.fetch(self.test_data), engine="h5netcdf")
         sfcWindmax, _ = atmos.wind_speed_from_vector(ds.uas, ds.vas)
 
         sfcWindmax_max = atmos.sfcWindmax_max(sfcWindmax)
         c = sfcWindmax.resample(time="YS").max()
         np.testing.assert_array_equal(sfcWindmax_max, c)
 
-
-class TestSfcWindmaxMean:
     def test_sfcWindmax_mean(self, nimbus):
-        ds = xr.open_dataset(nimbus.fetch("ERA5/daily_surface_cancities_1990-1993.nc"), engine="h5netcdf")
+        ds = xr.open_dataset(nimbus.fetch(self.test_data), engine="h5netcdf")
         sfcWindmax, _ = atmos.wind_speed_from_vector(ds.uas, ds.vas)
 
         sfcWindmax_mean = atmos.sfcWindmax_mean(sfcWindmax)
         c = sfcWindmax.resample(time="YS").mean()
         np.testing.assert_array_equal(sfcWindmax_mean, c)
 
-
-class TestSfcWindmaxMin:
-    def test_sfcWindmax_mean(self, nimbus):
-        ds = xr.open_dataset(nimbus.fetch("ERA5/daily_surface_cancities_1990-1993.nc"), engine="h5netcdf")
+    def test_sfcWindmax_min(self, nimbus):
+        ds = xr.open_dataset(nimbus.fetch(self.test_data), engine="h5netcdf")
         sfcWindmax, _ = atmos.wind_speed_from_vector(ds.uas, ds.vas)
 
         sfcWindmax_min = atmos.sfcWindmax_min(sfcWindmax)
