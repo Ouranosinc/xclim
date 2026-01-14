b715e26eb3a1aee5d1e51e862f78fa7c0b8677b9
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2026-01-14 12:26:28 -0500

    support older pylint, do not import matplotlib
    Signed-off-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>

--- src/xclim/testing/conftest.py
+++ src/xclim/testing/conftest.py
@@ -6,6 +6,7 @@
 
 from __future__ import annotations
 
+import importlib.util
 import os
 from pathlib import Path
 
@@ -90,10 +91,8 @@ def is_matplotlib_installed(xdoctest_namespace) -> None:  # numpydoc ignore=PR01
     """Skip tests that require matplotlib if it is not installed."""
 
     def _is_matplotlib_installed():
-        try:
-            import matplotlib  # noqa: F401
-
-        except ImportError:
+        matplotlib = importlib.util.find_spec("matplotlib")
+        if not hasattr(matplotlib, "origin"):
             pytest.skip("This doctest requires matplotlib to be installed.")
 
     xdoctest_namespace["is_matplotlib_installed"] = _is_matplotlib_installed
